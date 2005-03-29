49b2d7d77c3eceacc2a586e12f08aa84a6e43194
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2005-03-29 08:32:33 +0000

    Stability (junit suite passed) after a lot of hibernate version code added

--- trunk/OpenXava/generator/EJBeanPG.java
+++ trunk/OpenXava/generator/EJBeanPG.java
@@ -14,7 +14,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Mon Mar 28 12:17:38 CEST 2005
+ * @version Tue Mar 29 10:17:20 CEST 2005
  */
 public class EJBeanPG {
     Properties properties = new Properties();
@@ -1321,9 +1321,9 @@ private String generateEJBQLforReference(IMetaModel model, String referenceName)
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Mon Mar 28 12:17:39 CEST 2005", // date this file was generated
-             "/home/mcarmen/workspace/OpenXava/generator/ejbean.xml", // input file
-             "/home/mcarmen/workspace/OpenXava/generator/EJBeanPG.java" }, // output file
+        { "Tue Mar 29 10:17:21 CEST 2005", // date this file was generated
+             "/home/javi/workspace/OpenXava/generator/ejbean.xml", // input file
+             "/home/javi/workspace/OpenXava/generator/EJBeanPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:39:37 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:37:21 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
--- trunk/OpenXava/generator/PropertyPG.java
+++ trunk/OpenXava/generator/PropertyPG.java
@@ -13,7 +13,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Mon Mar 28 12:17:36 CEST 2005
+ * @version Tue Mar 29 10:17:17 CEST 2005
  */
 public class PropertyPG {
     Properties properties = new Properties();
@@ -600,9 +600,9 @@ private static void generate(XPathContext context, ProgramWriter out, MetaProper
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Mon Mar 28 12:17:36 CEST 2005", // date this file was generated
-             "/home/mcarmen/workspace/OpenXava/generator/property.xml", // input file
-             "/home/mcarmen/workspace/OpenXava/generator/PropertyPG.java" }, // output file
+        { "Tue Mar 29 10:17:18 CEST 2005", // date this file was generated
+             "/home/javi/workspace/OpenXava/generator/property.xml", // input file
+             "/home/javi/workspace/OpenXava/generator/PropertyPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:39:37 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:37:21 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
