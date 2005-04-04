d5056515cfa34ff94eae0e61dea797642d14df34
mcarmengimeno <mcarmengimeno@576487a9-6920-41e2-8c29-37f306983587>
2005-04-04 21:15:41 +0000

    Work in hibernate map facade

--- OpenXava/generator/EJBeanPG.java
+++ OpenXava/generator/EJBeanPG.java
@@ -14,7 +14,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Thu Mar 31 13:42:57 CEST 2005
+ * @version Sat Apr 02 10:56:54 CEST 2005
  */
 public class EJBeanPG {
     Properties properties = new Properties();
@@ -1321,9 +1321,9 @@ private String generateEJBQLforReference(IMetaModel model, String referenceName)
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Thu Mar 31 13:42:58 CEST 2005", // date this file was generated
-             "/home/javi/workspace/OpenXava/generator/ejbean.xml", // input file
-             "/home/javi/workspace/OpenXava/generator/EJBeanPG.java" }, // output file
+        { "Sat Apr 02 10:56:55 CEST 2005", // date this file was generated
+             "/home/mcarmen/workspace/OpenXava/generator/ejbean.xml", // input file
+             "/home/mcarmen/workspace/OpenXava/generator/EJBeanPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:39:37 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:37:21 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
