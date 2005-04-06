633481ded4d5f265d4c888323150d05e1c1832dd
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2005-04-06 14:51:22 +0000

    Bug on generate code if not use generateHibernate

--- trunk/OpenXava/generator/HibernatePG.java
+++ trunk/OpenXava/generator/HibernatePG.java
@@ -10,7 +10,7 @@ import org.openxava.util.Strings;
 
 /**
  * Program Generator created by TL2Java
- * @version Tue Apr 05 18:58:42 CEST 2005
+ * @version Wed Apr 06 16:46:50 CEST 2005
  */
 public class HibernatePG {
     Properties properties = new Properties();
@@ -134,7 +134,7 @@ public class HibernatePG {
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Tue Apr 05 18:58:42 CEST 2005", // date this file was generated
+        { "Wed Apr 06 16:46:50 CEST 2005", // date this file was generated
              "/home/javi/workspace/OpenXava/generator/hibernate.xml", // input file
              "/home/javi/workspace/OpenXava/generator/HibernatePG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
