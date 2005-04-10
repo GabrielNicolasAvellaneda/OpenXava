4518eecdeed986f5f7ccd9afaf5025e200085cfa
mcarmengimeno <mcarmengimeno@576487a9-6920-41e2-8c29-37f306983587>
2005-04-10 10:20:41 +0000

    MapFacade hibernate version run well for first time

--- OpenXavaTest/src/org/openxava/test/tests/FamiliesHibernateTest.java
+++ OpenXavaTest/src/org/openxava/test/tests/FamiliesHibernateTest.java
@@ -42,4 +42,36 @@ public class FamiliesHibernateTest extends ModuleTestBase {
 		execute("Mode.list");
 		assertListRowCount(3);
 	}
+	
+	public void testCreateReadUpdateDelete() throws Exception {
+		assertListRowCount(3);
+		// Create
+		execute ("CRUD.new");
+		setValue("number","66");
+		setValue("description","Family JUnit");
+		execute("CRUD.save");
+		assertNoErrors();
+		execute("Mode.list");
+		assertListRowCount(4);
+		// Read
+		execute("List.viewDetail", "row=3");
+		assertNoErrors();
+		assertValue("number","66");
+		assertValue("description","FAMILY JUNIT");
+		// Modify
+		setValue("description","Family JUnit Modified");
+		execute("CRUD.save");
+		assertNoErrors();
+		execute("Mode.list");
+		assertValueInList(3,"description","FAMILY JUNIT MODIFIED");
+		// Delete
+		execute("List.viewDetail", "row=3");
+		assertNoErrors();
+		execute("CRUD.delete");
+		execute("ConfirmDelete.confirmDelete");
+		assertMessage("Family deleted successfully");		
+		execute("Mode.list");
+		assertListRowCount(3);
+	}
+	
 }
