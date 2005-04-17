52017227bc9acb56b69104b160394a42e6f93db3
mcarmengimeno <mcarmengimeno@576487a9-6920-41e2-8c29-37f306983587>
2005-04-17 19:43:31 +0000

    In process refactoring of hibernate map facade.

--- trunk/OpenXavaTest/src/org/openxava/test/tests/FamiliesTest.java
+++ trunk/OpenXavaTest/src/org/openxava/test/tests/FamiliesTest.java
@@ -25,6 +25,36 @@ public class FamiliesTest extends ModuleTestBase {
 		super(testName, "OpenXavaTest", "Families");		
 	}
 
+	public void testCreateReadUpdateDelete() throws Exception {
+		assertListRowCount(3);
+		// Create
+		execute ("CRUD.new");
+		setValue("number","66");
+		setValue("description","Family JUnit");
+		execute("CRUD.save");
+		assertNoErrors();
+		execute("Mode.list");
+		assertListRowCount(4);
+		// Read
+		execute("List.viewDetail", "row=3");
+		assertNoErrors();
+		assertValue("number","66");
+		assertValue("description","FAMILY JUNIT");
+		// Modify
+		setValue("description","Family JUnit Modified");
+		execute("CRUD.save");
+		assertNoErrors();
+		execute("Mode.list");
+		assertValueInList(3,"description","FAMILY JUNIT MODIFIED");
+		// Delete
+		execute("List.viewDetail", "row=3");
+		assertNoErrors();
+		execute("CRUD.delete");
+		execute("ConfirmDelete.confirmDelete");
+		assertMessage("Family deleted successfully");		
+		execute("Mode.list");
+		assertListRowCount(3);
+	}
 	
 	public void testTabDefaultOrder() throws Exception {
 		assertActions(listActions);
