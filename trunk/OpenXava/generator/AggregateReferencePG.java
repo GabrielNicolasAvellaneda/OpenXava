1f0756a142ad8c0acf8570c9fa1c945beea31e67
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2005-05-20 14:39:46 +0000

    Before version

--- trunk/OpenXava/generator/AggregateReferencePG.java
+++ trunk/OpenXava/generator/AggregateReferencePG.java
@@ -13,7 +13,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Thu May 19 11:58:20 CEST 2005
+ * @version Fri May 20 16:36:34 CEST 2005
  */
 public class AggregateReferencePG {
     Properties properties = new Properties();
@@ -196,7 +196,7 @@ public static void generate(XPathContext context, ProgramWriter out, MetaReferen
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Thu May 19 11:58:20 CEST 2005", // date this file was generated
+        { "Fri May 20 16:36:34 CEST 2005", // date this file was generated
              "/home/javi/workspace/OpenXava/generator/aggregateReference.xml", // input file
              "/home/javi/workspace/OpenXava/generator/AggregateReferencePG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
