87706c0fea84656d7e6fd91db7bfb5330f4330ed
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2005-05-23 10:34:09 +0000

    Fix: null is displayed when you use combo with compose key and empty

--- trunk/OpenXavaTest/src/org/openxava/test/tests/ProductsWithSectionTest.java
+++ trunk/OpenXavaTest/src/org/openxava/test/tests/ProductsWithSectionTest.java
@@ -1,5 +1,7 @@
 package org.openxava.test.tests;
 
+import org.openxava.test.ejb.*;
+
 
 /**
  * @author Javier Paniza
@@ -9,6 +11,36 @@ public class ProductsWithSectionTest extends ProductsTest {
 		
 	public ProductsWithSectionTest(String testName) {
 		super(testName, "ProductsWithSection");		
+	}
+	
+	public void testEmptyCombosUsingComposeKeys() throws Exception {
+		execute("CRUD.new");
+		
+		WarehouseKey key1 = new WarehouseKey();
+		key1.setZoneNumber(1);
+		key1.set_Number(new Integer(1));
+		WarehouseKey key2 = new WarehouseKey();
+		key2.setZoneNumber(1);
+		key2.set_Number(new Integer(2));
+		WarehouseKey key3 = new WarehouseKey();
+		key3.setZoneNumber(1);
+		key3.set_Number(new Integer(3));		
+		
+		String [][] zone1WarehouseValues = new String [][] { // It is assumed that 1 is default zone
+			{ "", "" },
+			{ key1.toString(), "CENTRAL VALENCIA" },
+			{ key3.toString(), "VALENCIA NORTE" },
+			{ key2.toString(), "VALENCIA SURETE" }
+		};
+			
+		assertValidValues("warehouseKey", zone1WarehouseValues);
+		
+		execute("Products.changeDefaultZone");		
+		String [][] zoneEmptyWarehouseValues = new String [][] { 
+			{ "", "" },		
+		};		
+		assertValidValues("warehouseKey", zoneEmptyWarehouseValues);
+		
 	}
 	
 }
