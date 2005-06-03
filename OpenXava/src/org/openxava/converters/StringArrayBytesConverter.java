4d7126e906e6a9906263f4125325d4ca5c1c1448
lumiher <lumiher@576487a9-6920-41e2-8c29-37f306983587>
2005-06-03 10:10:21 +0000

    *** empty log message ***

---
+++ OpenXava/src/org/openxava/converters/StringArrayBytesConverter.java
@@ -0,0 +1,42 @@
+/*
+ * Created on 19-may-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.openxava.converters;
+
+import org.openxava.util.XavaResources;
+
+/**
+ * @author Luis Miguel
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class StringArrayBytesConverter implements IConverter {
+    public Object toJava(Object o) throws ConversionException {	    
+    	if (o == null) return "";
+    	System.out.println("[StringBlobConverter.toJava:] o.getClass():" + o.getClass().getName());
+	    if (!(o instanceof byte[])) {		
+			throw new ConversionException("conversion_java_blob_expected");
+		}
+		byte[] b = (byte[]) o;
+		try {
+			return new String(b);
+		}catch (Exception e){
+		    e.printStackTrace();
+			System.err.println(XavaResources.getString("blob_to_array_warning"));
+			return "";
+		}
+		
+	}
+	
+
+
+public Object toDB(Object o) throws ConversionException {
+    return o==null?null:o.toString().getBytes();
+    //StringValue value = new StringValue(o==null?"":o.toString());
+    //return value;
+}
+}
---
+++ OpenXava/src/org/openxava/converters/StringNotNullConverter.java
@@ -0,0 +1,18 @@
+package org.openxava.converters;
+
+/**
+ * @author Javier Paniza
+ */
+public class StringNotNullConverter implements IConverter {
+
+	public Object toJava(Object o) throws ConversionException {
+	    if (o == null) return "";
+		return o;
+	}
+
+	public Object toDB(Object o) throws ConversionException {
+	    if (o == null) return "";
+		return o;
+	}
+		
+}
