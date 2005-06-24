9d32de4a029b8bad4986d6bac7d1772cc6de8387
lumiher <lumiher@576487a9-6920-41e2-8c29-37f306983587>
2005-06-24 08:41:33 +0000

    *** empty log message ***

---
+++ trunk/OpenXava/src/org/openxava/calculators/BigDecimalCalculator.java
@@ -0,0 +1,27 @@
+package org.openxava.calculators;
+
+import java.math.BigDecimal;
+
+
+/**
+ * @author Javier Paniza
+ */
+public class BigDecimalCalculator implements ICalculator {
+
+	private Object value;
+
+	public Object calculate() throws Exception {		
+	    if (getValue()==null) return new BigDecimal(0);
+		return new BigDecimal(getValue().toString());
+	}
+
+	public Object getValue() {
+	    if (value==null) return new BigDecimal(0);
+		return value.toString();
+	}
+
+	public void setValue(Object i){
+		value = i;
+	}
+
+}
