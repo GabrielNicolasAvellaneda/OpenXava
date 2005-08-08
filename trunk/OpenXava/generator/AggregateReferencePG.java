6760f1c7806aa2c259ba921426fc509d8f88c0ef
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2005-08-08 06:37:20 +0000

    Lasted changes in hibernate version integrated and stable

--- trunk/OpenXava/generator/AggregateReferencePG.java
+++ trunk/OpenXava/generator/AggregateReferencePG.java
@@ -13,7 +13,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Wed Aug 03 18:24:24 CEST 2005
+ * @version Mon Aug 08 08:35:36 CEST 2005
  */
 public class AggregateReferencePG {
     Properties properties = new Properties();
@@ -205,9 +205,9 @@ public static void generate(XPathContext context, ProgramWriter out, MetaReferen
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Wed Aug 03 18:24:24 CEST 2005", // date this file was generated
-             "/home/mcarmen/workspace/OpenXava/generator/aggregateReference.xml", // input file
-             "/home/mcarmen/workspace/OpenXava/generator/AggregateReferencePG.java" }, // output file
+        { "Mon Aug 08 08:35:36 CEST 2005", // date this file was generated
+             "/home/javi/workspace/OpenXava/generator/aggregateReference.xml", // input file
+             "/home/javi/workspace/OpenXava/generator/AggregateReferencePG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:39:37 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:37:21 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
--- trunk/OpenXava/generator/EntityReferencePG.java
+++ trunk/OpenXava/generator/EntityReferencePG.java
@@ -13,7 +13,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Wed Aug 03 18:24:24 CEST 2005
+ * @version Mon Aug 08 08:35:34 CEST 2005
  */
 public class EntityReferencePG {
     Properties properties = new Properties();
@@ -371,9 +371,9 @@ public static void generate(XPathContext context, ProgramWriter out, MetaReferen
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Wed Aug 03 18:24:24 CEST 2005", // date this file was generated
-             "/home/mcarmen/workspace/OpenXava/generator/entityReference.xml", // input file
-             "/home/mcarmen/workspace/OpenXava/generator/EntityReferencePG.java" }, // output file
+        { "Mon Aug 08 08:35:35 CEST 2005", // date this file was generated
+             "/home/javi/workspace/OpenXava/generator/entityReference.xml", // input file
+             "/home/javi/workspace/OpenXava/generator/EntityReferencePG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:39:37 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:37:21 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
