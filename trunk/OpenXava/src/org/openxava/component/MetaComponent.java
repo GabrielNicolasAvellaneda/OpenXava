97ab6b540d5c8050a019dada5f546e2b04ef83dd
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2005-10-07 10:18:39 +0000

    Componente with duplicate name is not allowed. In order to elude commons mistakes

--- trunk/OpenXava/src/org/openxava/component/MetaComponent.java
+++ trunk/OpenXava/src/org/openxava/component/MetaComponent.java
@@ -52,7 +52,10 @@ public class MetaComponent implements Serializable {
 				throw new ElementNotFoundException("component_not_found", name);
 			}
 			r.validate();						
-			components.put(name, r);
+			Object previous = components.put(name, r);
+			if (previous != null) {
+				throw new XavaException("duplicate_component", name);
+			}
 		}		
 		return r;
 	}
