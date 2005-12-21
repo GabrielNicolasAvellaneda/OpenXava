1c3bc37f16c848a92eab069da5e37608e06df444
anafernandez <anafernandez@576487a9-6920-41e2-8c29-37f306983587>
2005-12-21 09:18:05 +0000

    Validador para propiedades BigDecimal

---
+++ OpenXava/src/org/openxava/validators/BigDecimalValidator.java
@@ -0,0 +1,60 @@
+package org.openxava.validators;
+
+import java.math.BigDecimal;
+import java.util.StringTokenizer;
+
+import org.openxava.util.Messages;
+import org.openxava.util.Strings;
+
+/**
+ * Created on 13-dic-2005 (10:09:52)
+ * @author Ana Andres
+ */
+public class BigDecimalValidator implements IPropertyValidator {
+    private int maximumIntegerDigits = 15;
+    private int maximumFractionDigits = 2;
+
+    public void validate(Messages errors, Object value, String propertyName, String modelName) throws Exception {
+        BigDecimal bigDecimal = (BigDecimal) value;
+        
+        int maximumValue = new Integer("1" + Strings.repeat(maximumIntegerDigits, "0")).intValue();
+        //
+        StringTokenizer st = new StringTokenizer(bigDecimal.toString(), ".");
+        int amount = st.countTokens();
+        int integer = 0;
+        int fraction = 0;
+        
+        integer = Integer.parseInt(st.nextToken());
+        if (amount > 1){
+            fraction = Integer.parseInt(st.nextToken());
+        }
+        //
+        if(integer > maximumValue){
+           errors.add("greaterThanTheAwaited", propertyName, modelName, String.valueOf(maximumValue)); 
+        }
+        //
+        if(fraction > 0){
+            int lengthFraction = String.valueOf(fraction).length();
+            if(lengthFraction > maximumFractionDigits){
+                errors.add("greaterNumberFraction", String.valueOf(getMaximumFractionDigits()), String.valueOf(lengthFraction));
+            }
+        }
+    }    
+    
+    public int getMaximumIntegerDigits() {
+        return maximumIntegerDigits;
+    }
+    
+    public void setMaximumIntegerDigits(int maximumIntegerDigits) {
+        this.maximumIntegerDigits = maximumIntegerDigits;
+    }
+    
+    public int getMaximumFractionDigits() {
+        return maximumFractionDigits;
+    }
+    
+    public void setMaximumFractionDigits(int maxinumFractionDigits) {
+        this.maximumFractionDigits = maxinumFractionDigits;
+    }
+
+}
