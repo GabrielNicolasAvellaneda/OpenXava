56e19997547dac9598c72f8f3b5ccbf0566cbe68
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2005-12-26 17:57:38 +0000

    Support for MapFacade.findEntity without lost the session for hibernate

--- OpenXavaTest/src/org/openxava/test/actions/TranslateCarrierNameAction.java
+++ OpenXavaTest/src/org/openxava/test/actions/TranslateCarrierNameAction.java
@@ -1,9 +1,12 @@
 package org.openxava.test.actions;
 
+import java.rmi.*;
 import java.util.*;
 
 import org.openxava.actions.*;
+import org.openxava.model.impl.*;
 import org.openxava.test.model.*;
+import org.openxava.util.*;
 
 
 
@@ -12,12 +15,12 @@ import org.openxava.test.model.*;
  */
 public class TranslateCarrierNameAction extends CollectionBaseAction {
 
-	public void execute() throws Exception {		
+	public void execute() throws Exception {
 		Iterator it = getSelectedObjects().iterator();
 		while (it.hasNext()) {
 			ICarrier carrier = (ICarrier) it.next();
 			carrier.translate();
-		}
+		}		
 	}
-
+	
 }
