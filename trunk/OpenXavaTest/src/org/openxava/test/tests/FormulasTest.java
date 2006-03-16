ba627fba43a5a4f9145daf2b9ffe63793d594749
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2006-03-16 17:51:37 +0000

    Fix: dependent references in collection do not work if its key are hidden

---
+++ trunk/OpenXavaTest/src/org/openxava/test/tests/FormulasTest.java
@@ -0,0 +1,48 @@
+package org.openxava.test.tests;
+
+import org.openxava.tests.*;
+
+
+
+/**
+ * @author Javier Paniza
+ */
+
+public class FormulasTest extends ModuleTestBase {
+	
+	public FormulasTest(String testName) {
+		super(testName, "OpenXavaTest", "Formulas");		
+	}
+	
+	public void testDependentReferencesAsDescriptionsListWithHiddenKeyInCollection() throws Exception {
+		execute("CRUD.new");
+		execute("Collection.new", "viewObject=xava_view_ingredients");
+		String [][] ingredients = {
+			{ "", "" },
+			{ "03C5C64CC0A80116000000009590B64C", "AZUCAR" },
+			{ "03C59CF0C0A8011600000000618CC74B", "CAFE" },
+			{ "03C6E1ADC0A8011600000000498BC537", "CAFE CON LECHE" },
+			{ "03C6B61AC0A8011600000000AB4E7ACB", "LECHE" }, 
+			{ "03C6C61DC0A801160000000076765581", "LECHE CONDENSADA"} 
+		};
+		
+		String [][] empty = {
+			{ "", "" }
+		};
+		
+		String [][] cafeConLeche = {
+				{ "", "" },
+				{ "03C5C64CC0A80116000000009590B64C", "AZUCAR" },
+				{ "03C59CF0C0A8011600000000618CC74B", "CAFE" },		
+				{ "03C6B61AC0A8011600000000AB4E7ACB", "LECHE" }, 		 				
+		};
+		
+		assertValidValues("ingredients.ingredient.oid", ingredients);
+		assertValidValues("ingredients.accentuate.oid", empty);
+		
+		setValue("ingredients.ingredient.oid", "03C6E1ADC0A8011600000000498BC537");
+		assertValidValues("ingredients.ingredient.oid", ingredients);
+		assertValidValues("ingredients.accentuate.oid", cafeConLeche);
+	}
+		
+}
