1a27cc0a6f655e9fa22cb7874cd6bf8f952b83eb
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2006-03-27 09:58:07 +0000

    Dependencies on EJB code removed and all tested and integrated

--- OpenXava/generator/EntityReferenceEJBPG.java
+++ OpenXava/generator/EntityReferenceEJBPG.java
@@ -12,7 +12,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Wed Mar 22 13:32:10 CET 2006
+ * @version Fri Mar 24 18:56:02 CET 2006
  */
 public class EntityReferenceEJBPG {
     Properties properties = new Properties();
@@ -417,7 +417,7 @@ public static void generate(XPathContext context, ProgramWriter out, MetaReferen
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Wed Mar 22 13:32:11 CET 2006", // date this file was generated
+        { "Fri Mar 24 18:56:03 CET 2006", // date this file was generated
              "/home/javi/workspace/OpenXava/generator/entityReferenceEJB.xml", // input file
              "/home/javi/workspace/OpenXava/generator/EntityReferenceEJBPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
--- OpenXava/generator/PropertyPG.java
+++ OpenXava/generator/PropertyPG.java
@@ -13,7 +13,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Wed Mar 22 13:32:09 CET 2006
+ * @version Fri Mar 24 18:56:01 CET 2006
  */
 public class PropertyPG {
     Properties properties = new Properties();
@@ -631,7 +631,7 @@ private static void generate(XPathContext context, ProgramWriter out, MetaProper
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Wed Mar 22 13:32:09 CET 2006", // date this file was generated
+        { "Fri Mar 24 18:56:01 CET 2006", // date this file was generated
              "/home/javi/workspace/OpenXava/generator/property.xml", // input file
              "/home/javi/workspace/OpenXava/generator/PropertyPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
