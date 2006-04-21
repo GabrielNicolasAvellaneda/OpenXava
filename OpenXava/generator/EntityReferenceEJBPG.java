a0c71fe07703ecbe820481bbe5fc8808a84a1771
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2006-04-21 08:58:08 +0000

    Refactoring of model.meta and model.impl are tested with Hibernate and EJB

--- OpenXava/generator/EntityReferenceEJBPG.java
+++ OpenXava/generator/EntityReferenceEJBPG.java
@@ -12,7 +12,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Thu Apr 20 19:07:29 CEST 2006
+ * @version Fri Apr 21 10:32:24 CEST 2006
  */
 public class EntityReferenceEJBPG {
     Properties properties = new Properties();
@@ -421,9 +421,9 @@ public static void generate(XPathContext context, ProgramWriter out, MetaReferen
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Thu Apr 20 19:07:29 CEST 2006", // date this file was generated
-             "/home/javi/workspace2/OpenXava/generator/entityReferenceEJB.xml", // input file
-             "/home/javi/workspace2/OpenXava/generator/EntityReferenceEJBPG.java" }, // output file
+        { "Fri Apr 21 10:32:25 CEST 2006", // date this file was generated
+             "/home/javi/workspace/OpenXava/generator/entityReferenceEJB.xml", // input file
+             "/home/javi/workspace/OpenXava/generator/EntityReferenceEJBPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:39:37 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
         {"Mon Apr 09 16:37:21 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
