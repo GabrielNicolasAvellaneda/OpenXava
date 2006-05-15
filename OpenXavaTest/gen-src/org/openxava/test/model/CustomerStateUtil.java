84a9c46bb61eba781efffbcb4c6a02d7854c6853
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2006-05-15 09:10:59 +0000

    Updating after Sourceforge CVS update

---
+++ OpenXavaTest/gen-src/org/openxava/test/model/CustomerStateUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Generated file - Do not edit!
+ */
+package org.openxava.test.model;
+
+import org.openxava.ejbx.*;
+
+/**
+ * Utility class for CustomerState.
+ */
+public class CustomerStateUtil
+{
+   /** Cached remote home (EJBHome). Uses lazy loading to obtain its value (loaded by getHome() methods). */
+   private static org.openxava.test.model.CustomerStateHome cachedRemoteHome = null;
+
+   private static Object lookupHome(java.util.Hashtable environment, String jndiName, Class narrowTo) throws javax.naming.NamingException {
+      // Obtain initial context
+      IContext initialContext = BeansContext.get();
+      try {
+         Object objRef = initialContext.lookup(jndiName);
+         // only narrow if necessary
+         // if (narrowTo.isInstance(java.rmi.Remote.class)) // this does not like much to websphere 6
+            return javax.rmi.PortableRemoteObject.narrow(objRef, narrowTo);
+         /*else
+            return objRef;*/
+      } finally {
+         initialContext.close();
+      }
+   }
+
+   // Home interface lookup methods
+
+   /**
+    * Obtain remote home interface from default initial context
+    * @return Home interface for CustomerState. Lookup using JNDI_NAME
+    */
+   public static org.openxava.test.model.CustomerStateHome getHome() throws javax.naming.NamingException
+   {
+      if (cachedRemoteHome == null) {
+            cachedRemoteHome = (org.openxava.test.model.CustomerStateHome) lookupHome(null, org.openxava.test.model.CustomerStateHome.JNDI_NAME, org.openxava.test.model.CustomerStateHome.class);
+      }
+      return cachedRemoteHome;
+   }
+
+   /**
+    * Obtain remote home interface from parameterised initial context
+    * @param environment Parameters to use for creating initial context
+    * @return Home interface for CustomerState. Lookup using JNDI_NAME
+    */
+   public static org.openxava.test.model.CustomerStateHome getHome( java.util.Hashtable environment ) throws javax.naming.NamingException
+   {
+       return (org.openxava.test.model.CustomerStateHome) lookupHome(environment, org.openxava.test.model.CustomerStateHome.JNDI_NAME, org.openxava.test.model.CustomerStateHome.class);
+   }
+
+}
+
--- OpenXavaTest/src/org/openxava/test/model/Family.java
+++ OpenXavaTest/src/org/openxava/test/model/Family.java
@@ -7,11 +7,43 @@ import org.openxava.model.meta.*;
 import org.openxava.util.*;
 
 /**
+ * Handmade POJO class for Family. <p>
+ * 
+ * Implementing IFamily is not mandatory,
+ * but if you does not implement it, you cannot
+ * mix generated and handmade code with references.<p>
+ * 
  * @author Javier Paniza
  */
 public class Family implements IFamily, Serializable {
 	
-	private MetaModel metaModel;
+	
+	private String oid;
+	private int number;
+	private String description;
+	
+	public String getOid() {
+		return oid;
+	}
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+		
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	private static MetaModel metaModel;
 	public MetaModel getMetaModel() throws XavaException {
 		if (metaModel == null) {
 			metaModel = MetaComponent.get("Family").getMetaEntity(); 	
@@ -19,26 +51,5 @@ public class Family implements IFamily, Serializable {
 		return metaModel;
 	}
 	
-
-	private String oid;
-	private int number;
-	private String description;
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public int getNumber() {
-		return number;
-	}
-	public void setNumber(int number) {
-		this.number = number;
-	}
-	public String getOid() {
-		return oid;
-	}
-	public void setOid(String oid) {
-		this.oid = oid;
-	}
+	
 }
