39717269f204f92e5c439c9ff97072412967891b
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2006-06-08 10:13:26 +0000

    EJB3 JPA now can be used to implements your actions

---
+++ OpenXava/src/org/openxava/jpa/XPersistence.java
@@ -0,0 +1,134 @@
+package org.openxava.jpa;
+
+import javax.persistence.*;
+
+/**
+ * Allows to work easily with EJB3 JPA inside OpenXava applications. <p>
+ * 
+ * You can use this class in any part of your OpenXava application:
+ * calculators, validators, actions, junit tests, etc.<br>
+ * To use JPA with this class you can write something as:
+ * <pre>
+ * Invoice invoice = new Invoice();
+ * XPersistence.getManager().persist(invoice);
+ * </pre>
+ * 
+ * And no more.<br>
+ * The method getManager() creates a session and transaction the first time 
+ * in the thread, and the OpenXava close the manager at the end of action execution.<br>
+ * Also the next code is legal:
+ * <pre>
+ * Invoice invoice = new Invoice();
+ * XPersistence.getManager().persist(invoice);
+ * XPersistence.commit(); // In this way you commit and close manually.
+ * ...
+ * XPersistence.getManager().persist(customer); // As manager has been closed, a new one is created
+ * </pre> 
+ * 
+ * @author Javier Paniza
+ */
+
+public class XPersistence {
+
+	private static EntityManagerFactory entityManagerFactory; 
+	private static String persistenceUnitName = "hibernate";	
+	private static ThreadLocal currentManager = new ThreadLocal();		 
+
+	/**
+	 * <code>EntityManager</code> associated to current thread. <p>
+	 * 
+	 * If no manager exists or it's closed, then create a new one, and start a transaction.<br>
+	 *
+	 * @return Not null
+	 */
+	public static EntityManager getManager() {		
+		EntityManager s = (EntityManager) currentManager.get();
+		if (s == null || !s.isOpen()) {
+			s = openManager();
+		}		
+		return s;
+	}
+	
+	/**
+	 * Create a new <code>EntityManager</code>. <p>
+	 * 
+	 * This manager is not associated with the current thread,
+	 * and no transaction is started.
+	 *
+	 * @return Not null
+	 */
+	public static EntityManager createManager() {
+		return getEntityManagerFactory().createEntityManager();
+	}
+				
+	private static EntityManager openManager() {
+		EntityManager m = getEntityManagerFactory().createEntityManager();
+		m.getTransaction().begin();		
+		currentManager.set(m);
+		return m;
+	}
+	
+	/**
+	 * Commits changes and closes the <code>EntityManager</code> associated to 
+	 * the current thread. <p>
+	 * 
+	 * If no manager or it is closed this method does nothing.<br>
+	 * In most cases this method is called by OpenXava automatically, 
+	 * hence the programmer that uses JPA APIs does not need to call it.
+	 */
+	public static void commit() {		
+		EntityManager m = (EntityManager) currentManager.get();
+		if (m == null) return;
+		if (m.isOpen()) {			
+			EntityTransaction t = (EntityTransaction) m.getTransaction();			
+			if (t != null) t.commit();
+			else m.flush(); 
+			m.close();
+		}
+							
+		currentManager.set(null);				
+	}
+	
+	/**
+	 * Rollback changes and closes the <code>EntityManager</code> associated to 
+	 * the current thread. <p>
+	 * 
+	 * If no manager or it is closed this method does nothing.<br>
+	 */
+	public static void rollback() {
+		EntityManager m = (EntityManager) currentManager.get();
+		if (m == null) return;
+		if (m.isOpen()) {
+			EntityTransaction t = (EntityTransaction) m.getTransaction();
+			if (t != null) t.rollback(); // tmp: ¿in this way?
+			m.close();
+		}						
+		currentManager.set(null);					
+	}	
+	
+	private static EntityManagerFactory getEntityManagerFactory() {
+		if (entityManagerFactory == null) {
+			entityManagerFactory = Persistence.createEntityManagerFactory(getPersistenceUnitName());
+		}
+		return entityManagerFactory; 
+	}
+
+	/**
+	 * By default is <code>hibernate</code>. <p>
+	 * 
+	 * You must set value to this property before use any othe method of this class. 
+	 */
+	public static String getPersistenceUnitName() {
+		return persistenceUnitName;
+	}
+
+	/**
+	 * By default is <code>hibernate</code>. <p>
+	 * 
+	 * You must set value to this property before use any othe method of this class. 
+	 */	
+	public static void setPersistenceUnitName(String configurationFile) {
+		XPersistence.persistenceUnitName = configurationFile;
+	}
+		
+}
---
+++ OpenXavaTest/src/org/openxava/test/tests/StateJPATest.java
@@ -0,0 +1,48 @@
+package org.openxava.test.tests;
+
+import org.openxava.tests.*;
+
+/**
+ * @author Mª Carmen Gimeno
+ */
+
+public class StateJPATest extends ModuleTestBase {
+	
+	
+	public StateJPATest(String testName) {
+		super(testName, "OpenXavaTest", "StatesJPA");		
+	}
+
+	public void testCreateReadUpdateDeleteWithHandmadeJPA() throws Exception {
+		assertListNotEmpty();
+		// Create		
+		execute ("StatesJPA.new");
+		setValue("id","66");
+		setValue("name","State JUnit");
+		execute("StatesJPA.save");
+		assertNoErrors();
+		assertValue("id", "");
+		assertValue("name", "");
+		
+		// Read
+		setValue("id", "66");
+		execute("StatesJPA.search");
+		assertNoErrors();
+		assertValue("id","66");
+		assertValue("name","STATE JUNIT");
+		
+		// Modify
+		setValue("name","State JUnit Modified");
+		execute("StatesJPA.save");
+		assertNoErrors();
+		setValue("id", "66");
+		execute("StatesJPA.search");
+		assertNoErrors();
+		assertValue("id","66");
+		assertValue("name","STATE JUNIT MODIFIED");
+		
+		// Delete		
+		execute("StatesJPA.delete");
+		assertNoErrors();
+	}
+}
