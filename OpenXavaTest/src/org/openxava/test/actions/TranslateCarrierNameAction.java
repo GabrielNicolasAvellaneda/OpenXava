783806c82d88d00648093ad99ecff05349b6dda6
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2006-07-21 10:04:06 +0000

    add/removeListAction and add/removeDetailAction added to view. This allow add/remove programmatically actions to a collection view.

--- OpenXavaTest/src/org/openxava/test/actions/TranslateCarrierNameAction.java
+++ OpenXavaTest/src/org/openxava/test/actions/TranslateCarrierNameAction.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.openxava.actions.*;
 import org.openxava.test.model.*;
+import org.openxava.util.*;
 
 
 
@@ -11,13 +12,49 @@ import org.openxava.test.model.*;
  * @author Javier Paniza
  */
 public class TranslateCarrierNameAction extends CollectionBaseAction {
+	
+	private boolean all = false;
+	private String targetLanguage;
+	
 
 	public void execute() throws Exception {
-		Iterator it = getSelectedObjects().iterator();
+		Iterator it = (all?getObjects():getSelectedObjects()).iterator();
 		while (it.hasNext()) {
 			ICarrier carrier = (ICarrier) it.next();
-			carrier.translate();
+			// An 'carrier.translate(targetLanguage)' is a good alterantive 
+			if ("ES".equalsIgnoreCase(targetLanguage)) carrier.translateToSpanish();
+			else if ("EN".equalsIgnoreCase(targetLanguage)) carrier.translateToEnglish();
+			else carrier.translate();
 		}		
+		if ("EN".equalsIgnoreCase(targetLanguage)) {
+			getCollectionElementView().addListAction("Carriers.todosAEspanol");
+			getCollectionElementView().removeListAction("Carriers.allToEnglish");
+		}
+		else if ("ES".equalsIgnoreCase(targetLanguage)) {
+			getCollectionElementView().addListAction("Carriers.allToEnglish");
+			getCollectionElementView().removeListAction("Carriers.todosAEspanol");
+		}
 	}
+
+	public boolean isAll() {
+		return all;
+	}
+
+	public void setAll(boolean all) {
+		this.all = all;
+	}
+	
+	public String getTargetLanguage() {
+		return targetLanguage;
+	}
+
+	public void setTargetLanguage(String targetLanguage) {
+		if (!(Is.empty(targetLanguage) || 
+			"ES".equalsIgnoreCase(targetLanguage) ||
+			"EN".equalsIgnoreCase(targetLanguage))) {
+			throw new IllegalArgumentException("Only 'ES' or 'EN' are allowed for 'targetLanguage'");
+		}
+		this.targetLanguage = targetLanguage;
+	}	
 	
 }
--- OpenXavaTest/src/org/openxava/test/calculators/TranslateCarrierNameCalculator.java
+++ OpenXavaTest/src/org/openxava/test/calculators/TranslateCarrierNameCalculator.java
@@ -4,6 +4,7 @@ import java.rmi.*;
 
 import org.openxava.calculators.*;
 import org.openxava.test.model.*;
+import org.openxava.util.*;
 
 /**
  * @author Javier Paniza
@@ -11,27 +12,46 @@ import org.openxava.test.model.*;
 
 public class TranslateCarrierNameCalculator implements IModelCalculator {
 	
-	private ICarrier carrier;	
+	private ICarrier carrier;
+	private String targetLanguage;
 
 	public Object calculate() throws Exception {				
 		String name = carrier.getName();
 		String translated = name;
-		if ("UNO".equals(name)) translated = "ONE";
-		else if ("DOS".equals(name)) translated ="TWO";
-		else if ("TRES".equals(name)) translated ="THREE";
-		else if ("CUATRO".equals(name)) translated ="FOUR";
-		else if ("CINCO".equals(name)) translated ="FIVE";
-		else if ("ONE".equals(name)) translated = "UNO";
-		else if ("TWO".equals(name)) translated ="DOS";
-		else if ("THREE".equals(name)) translated ="TRES";
-		else if ("FOUR".equals(name)) translated ="CUATRO";
-		else if ("FIVE".equals(name)) translated ="CINCO";
+		if (Is.emptyString(targetLanguage) || "EN".equalsIgnoreCase(targetLanguage)) {
+			if ("UNO".equals(name)) translated = "ONE";
+			else if ("DOS".equals(name)) translated ="TWO";
+			else if ("TRES".equals(name)) translated ="THREE";
+			else if ("CUATRO".equals(name)) translated ="FOUR";
+			else if ("CINCO".equals(name)) translated ="FIVE";
+		}
+		
+		if (Is.emptyString(targetLanguage) || "ES".equalsIgnoreCase(targetLanguage)) {
+			if ("ONE".equals(name)) translated = "UNO";
+			else if ("TWO".equals(name)) translated ="DOS";
+			else if ("THREE".equals(name)) translated ="TRES";
+			else if ("FOUR".equals(name)) translated ="CUATRO";
+			else if ("FIVE".equals(name)) translated ="CINCO";
+		}
 		carrier.setName(translated);
 		return null;
 	}
 
 	public void setModel(Object entity) throws RemoteException {
 		this.carrier = (ICarrier) entity;		
+	}
+
+	public String getTargetLanguage() {
+		return targetLanguage;
+	}
+
+	public void setTargetLanguage(String targetLanguage) {
+		if (!(Is.empty(targetLanguage) || 
+			"ES".equalsIgnoreCase(targetLanguage) ||
+			"EN".equalsIgnoreCase(targetLanguage))) {
+			throw new IllegalArgumentException("Only 'ES' or 'EN' are allowed for 'targetLanguage'");
+		}
+		this.targetLanguage = targetLanguage;
 	}
 
 }
