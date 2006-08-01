eea13e94e039832d79bce572c055f94a221c3a86
maembuena <maembuena@576487a9-6920-41e2-8c29-37f306983587>
2006-08-01 08:43:44 +0000

    - getCriteriaSearchValues() in SearchByViewKeyAction is renamed to getValuesFromView()
    - added getKeyValuesFromView() and getMemberNames() methods in SearchByViewKeyAction

--- OpenXava/src/org/openxava/actions/SearchByViewKeyAction.java
+++ OpenXava/src/org/openxava/actions/SearchByViewKeyAction.java
@@ -1,11 +1,11 @@
 package org.openxava.actions;
 
-import java.util.*;
+import java.util.Map;
 
-import javax.ejb.*;
+import javax.ejb.ObjectNotFoundException;
 
-import org.openxava.model.*;
-import org.openxava.util.*;
+import org.openxava.model.MapFacade;
+import org.openxava.util.Maps;
 
 /**
  * @author Javier Paniza
@@ -13,22 +13,23 @@ import org.openxava.util.*;
 
 public class SearchByViewKeyAction extends ViewBaseAction {
 	
+	private static final long serialVersionUID = 1L;
+
 	public void execute() throws Exception {
-		try {			
-			Map memberNames = getView().getMembersNamesWithHidden();
+		try {						
 			Map keys = getView().getKeyValues();
 			Map values = null;
 			if (Maps.isEmptyOrZero(keys)) { 
 				try {
-					values = MapFacade.getValuesByAnyProperty(getModelName(), getCriteriaSearchValues(), memberNames);
+					values = MapFacade.getValuesByAnyProperty(getModelName(), getValuesFromView(), getMemberNames());
 				}
 				catch (ObjectNotFoundException ex) {
 					// This is for the case of key with 0 as valid value
-					values = MapFacade.getValues(getModelName(), keys, memberNames);
+					values = MapFacade.getValues(getModelName(), keys, getMemberNames());
 				}
 			}
 			else {				
-				values = MapFacade.getValues(getModelName(), keys, memberNames);
+				values = MapFacade.getValues(getModelName(), keys, getMemberNames());
 			}
 			getView().setEditable(true);	
 			getView().setKeyEditable(false);			
@@ -47,15 +48,28 @@ public class SearchByViewKeyAction extends ViewBaseAction {
 	protected void setValuesToView(Map values) throws Exception {
 		getView().setValues(values);
 	}
+
+	/** 
+	 * Allows to refine the members that will be including for the search of the object 
+	 */
+	protected Map getMemberNames() throws Exception{
+		return getView().getMembersNamesWithHidden();
+	}
 	
 	/**
-	 * The values used in order to prepare the searh condition. <p>
-	 * 
+	 * Allows to refine the values received from the view, in order to prepare the searh condition<br>
 	 * Blank and zeroes are ignored.<br>
 	 * By default assumed all data currently displayed to the user.<br>
 	 */
-	protected Map getCriteriaSearchValues() throws Exception {
+	protected Map getValuesFromView() throws Exception {
 		return getView().getValues();
+	}
+
+	/**
+	 * Allows to refine the key values received from the view<br> 
+	 */
+	protected Map getKeyValuesFromView() throws Exception {
+		return getView().getKeyValues();
 	}
 
 				
