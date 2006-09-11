7daff1841ed21bfe539e54bb97abc283143f98f4
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2006-09-11 15:53:32 +0000

    JPA code in POJOs only is generated if JPA persistence provider is set, or the property 'jpaCodeInPOJOs' in xava.properties is set to true. In this way POJOs can be compiled using a java 1.4 compiler

--- trunk/OpenXava/generator/BeanPG.java
+++ trunk/OpenXava/generator/BeanPG.java
@@ -9,7 +9,7 @@ import org.openxava.component.MetaComponent;
 
 /**
  * Program Generator created by TL2Java
- * @version Mon Aug 14 13:45:53 CEST 2006
+ * @version Mon Sep 11 17:39:21 CEST 2006
  */
 public class BeanPG {
     Properties properties = new Properties();
@@ -156,7 +156,7 @@ public class BeanPG {
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Mon Aug 14 13:45:54 CEST 2006", // date this file was generated
+        { "Mon Sep 11 17:39:21 CEST 2006", // date this file was generated
              "../OpenXava/generator/bean.xml", // input file
              "../OpenXava/generator/BeanPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
--- trunk/OpenXava/generator/EntityReferenceEJBPG.java
+++ trunk/OpenXava/generator/EntityReferenceEJBPG.java
@@ -12,7 +12,7 @@ import org.openxava.mapping.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Mon Aug 14 13:45:44 CEST 2006
+ * @version Mon Sep 11 17:39:16 CEST 2006
  */
 public class EntityReferenceEJBPG {
     Properties properties = new Properties();
@@ -421,7 +421,7 @@ public static void generate(XPathContext context, ProgramWriter out, MetaReferen
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Mon Aug 14 13:45:44 CEST 2006", // date this file was generated
+        { "Mon Sep 11 17:39:16 CEST 2006", // date this file was generated
              "../OpenXava/generator/entityReferenceEJB.xml", // input file
              "../OpenXava/generator/EntityReferenceEJBPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
