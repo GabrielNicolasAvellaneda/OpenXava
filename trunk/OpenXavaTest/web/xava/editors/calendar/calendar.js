697213ea4a21290069df36116ba875e87229543c
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-01-10 12:00:20 +0000

    Just before OpenXava_20070110

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.5 2007-01-10 10:56:53 javierpaniza Exp $
+// $Id: calendar.js,v 1.6 2007-01-10 11:58:41 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
