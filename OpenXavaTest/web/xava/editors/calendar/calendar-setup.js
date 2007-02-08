f96e3b1f81e36cffc7f356f813e0140ac581f158
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-02-08 19:02:04 +0000

    Tested with these configurations: * Tomcat + Hibernate * JBoss + EJB2 * Tomcat + JPA * JBoss + EJB + Hibernate * Tomcat + AS400 * Websphere 5.1 + Hib * Jetspeed 2 * Liferay * Tomca + Java 6 * Websphere Portal

--- OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
+++ OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
@@ -19,7 +19,7 @@
  * than modifying calendar.js itself).
  */
 
-// $Id: calendar-setup.js,v 1.12 2007-01-26 12:54:51 javierpaniza Exp $
+// $Id: calendar-setup.js,v 1.13 2007-02-08 19:02:04 javierpaniza Exp $
 
 /**
  *  This function "patches" an input field (or other element) to use a calendar
--- OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.12 2007-01-26 12:54:50 javierpaniza Exp $
+// $Id: calendar.js,v 1.13 2007-02-08 19:02:04 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
