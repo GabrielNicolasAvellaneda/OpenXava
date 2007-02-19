8f6847c647250b445255f29f8c6c2bac992ac77c
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-02-19 15:12:29 +0000

    Using key properties of parent model in a 'from' attribute from an aggregate works. That is, to use from="invoice.year" from a InvoiceDetail aggregate is allowed

--- OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.15 2007-02-19 11:14:48 javierpaniza Exp $
+// $Id: calendar.js,v 1.16 2007-02-19 15:12:29 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
