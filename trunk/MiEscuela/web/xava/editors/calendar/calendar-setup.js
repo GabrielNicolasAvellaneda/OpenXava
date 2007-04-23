e582d73fa268b2af59af41cc260a91ca2f7e927e
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-04-23 09:10:09 +0000

    Quitamos mapeo de Profesor.xml

--- trunk/MiEscuela/web/xava/editors/calendar/calendar-setup.js
+++ trunk/MiEscuela/web/xava/editors/calendar/calendar-setup.js
@@ -19,7 +19,7 @@
  * than modifying calendar.js itself).
  */
 
-// $Id: calendar-setup.js,v 1.2 2007-02-09 12:56:38 javierpaniza Exp $
+// $Id: calendar-setup.js,v 1.3 2007-04-23 09:09:50 javierpaniza Exp $
 
 /**
  *  This function "patches" an input field (or other element) to use a calendar
