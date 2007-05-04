d7c26558780eed542a856b6a959d42c0c62e1638
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-05-04 10:38:47 +0000

    Fix: In websphere 6.0 using WebSphereTransactionManagerLookup a lot of warning messages is shown on commit transaction.

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.21 2007-04-30 17:28:43 javierpaniza Exp $
+// $Id: calendar.js,v 1.22 2007-05-04 10:38:22 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
