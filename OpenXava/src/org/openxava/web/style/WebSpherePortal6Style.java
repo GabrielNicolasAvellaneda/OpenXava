9378a9e3af7a823b07a76eb8e2a27390295d9c61
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-06-05 09:09:32 +0000

    WebSphere Portal 6 style improved: Sections and frames have native aspect; and a little problem with resolutions higher than 1600x has been fixed

---
+++ OpenXava/src/org/openxava/web/style/WebSpherePortal6Style.java
@@ -0,0 +1,90 @@
+package org.openxava.web.style;
+
+/**
+ * 
+ * @author Javier Paniza
+ */
+
+public class WebSpherePortal6Style extends WebSpherePortalStyle {
+	
+	private static WebSpherePortal6Style instance = null;
+	
+	protected WebSpherePortal6Style() {		
+	}
+	
+	public static Style getInstance() {
+		if (instance == null) {
+			instance = new WebSpherePortal6Style();
+		}
+		return instance;
+	}
+
+	public String getSection() {
+		return "";
+	}	
+	
+	public String getSectionTableAttributes() {		
+		return "border='0' cellpadding='0' cellspacing='0' width='100%'";
+	}
+	
+	
+	public String getSectionActive() {
+		return "";
+	}	
+		
+	public String getSectionLink() {
+		return "";
+	}
+	
+	public String getSectionBarStartDecoration() {
+		return "<td><ul class='wpsPageBarFirstRow'>";
+	}
+	public String getSectionBarEndDecoration() {	
+		return "</ul><div class='pageBarSeparator'><!--  --></div></td>";
+	}
+		
+	public String getActiveSectionTabStartDecoration() {
+		return "<li class='wpsSelectedPage'>";
+	}
+	
+	public String getActiveSectionTabEndDecoration() {
+		return "</li>";		
+	}
+	
+	public String getSectionTabStartDecoration() {
+		return "<li class='wpsUnSelectedPage'>";
+	}
+	
+	public String getSectionTabEndDecoration() {
+		return "</li>";		
+	}
+
+	public String getFrame() {
+		return "wpsPortlet";
+	}
+	
+	public String getFrameTitle() {
+		return "wpsPortletTitleBar";
+	}
+		
+	public String getFrameTitleLabel() {
+		return "websphere-frame-title-label";
+	}
+	
+	public String getFrameTitleStartDecoration() {
+		return "<div style='width:100%;'>";
+	}
+	
+	public String getFrameTitleEndDecoration() {
+		return "<img alt='' style='border:0; text-align: right;' width='1' height='22' src='/wps/skins/html/IBM/title_minheight.gif'></div>";
+	}
+	
+	public String getFrameContent() {
+		return "wpsPortletBody";
+	}
+	
+	public String getFrameSpacing() {
+		return "border=0 cellspacing=0 cellpadding=2"; 		
+	}
+				
+}
