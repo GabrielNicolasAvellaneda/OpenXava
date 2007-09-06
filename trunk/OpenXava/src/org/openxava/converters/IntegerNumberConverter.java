a87cd04f815f5f858c82dbfd797d65793b172e7a
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-09-06 11:17:32 +0000

    New property nullToZero in IntegerNumberConverter and LongNumberConverter to choose if null have to be converted to 0.

--- trunk/OpenXava/src/org/openxava/converters/IntegerNumberConverter.java
+++ trunk/OpenXava/src/org/openxava/converters/IntegerNumberConverter.java
@@ -12,8 +12,9 @@ package org.openxava.converters;
  */
 public class IntegerNumberConverter implements IConverter {
 	
-	private final static Integer CERO = new Integer(0);
+	private final static Integer ZERO = new Integer(0);
 	private static IConverter instance;
+	private boolean nullToZero = true;
 	
 	public static IConverter getInstance() {
 		if (instance == null) {
@@ -23,15 +24,33 @@ public class IntegerNumberConverter implements IConverter {
 	}
 
 	public Object toDB(Object o) throws ConversionException {
-		return o==null?CERO:o;
+		return o==null && isNullToZero()?ZERO:o;
 	}
 	
 	public Object toJava(Object o) throws ConversionException {
-		if (o == null) return CERO;				
+		if (o == null) return isNullToZero()?ZERO:null;				
 		if (!(o instanceof Number)) {		
 			throw new ConversionException("conversion_java_number_expected");
 		}
 		return new Integer(((Number) o).intValue());
+	}
+
+	/**
+	 * If <code>true</code> convert null to zero. <p>
+	 * 
+	 * The default value is true.
+	 */
+	public boolean isNullToZero() {
+		return nullToZero;
+	}
+
+	/**
+	 * If <code>true</code> convert null to zero. <p>
+	 * 
+	 * The default value is true.
+	 */	
+	public void setNullToZero(boolean nullToZero) {
+		this.nullToZero = nullToZero;
 	}
 			
 }
