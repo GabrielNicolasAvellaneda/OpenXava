2392443cf5fae59789dc194b89f0903b4c849c1f
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2007-09-25 08:41:14 +0000

    EmailValidator uses commons validator

--- trunk/OpenXava/src/org/openxava/validators/EmailValidator.java
+++ trunk/OpenXava/src/org/openxava/validators/EmailValidator.java
@@ -1,10 +1,8 @@
 package org.openxava.validators;
 
+import org.apache.commons.validator.*;
 import org.openxava.util.*;
 import org.openxava.validators.IPropertyValidator;
-
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 /**
  * 
@@ -13,15 +11,12 @@ import java.util.regex.Matcher;
 
 public class EmailValidator implements IPropertyValidator {
 
-    final private Pattern pat=Pattern.compile(".+@.+\\.[a-z]+");
-
     public void validate(Messages errors, Object value, String propertyName, String modelName) throws Exception {
-        if (value == null || value.toString().length() ==0 ) return;
-        Matcher matcher = pat.matcher(value.toString());
-        if (! matcher.find()) {
-           errors.add("email_validation_error", propertyName);
-           return;
-        }
+    	if (value == null || value.toString().length() == 0) return;     	 
+    	if (! GenericValidator.isEmail(value.toString())) { 
+    		errors.add("email_validation_error", propertyName); 
+    		return; 
+    	}     
     }
 
 }
