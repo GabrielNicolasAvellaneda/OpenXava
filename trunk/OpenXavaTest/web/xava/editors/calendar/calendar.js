ae4dd9c789f16a1e8f985d9f79baee7ddc5ba455
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2008-05-27 11:02:16 +0000

    Java Preferences used for storing user preferences. OpenXavaDS and openxava-db are no longer needed

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.73 2008-05-12 08:46:36 javierpaniza Exp $
+// $Id: calendar.js,v 1.74 2008-05-27 11:01:49 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
