b65fdad51ebd491e43f7702097c779a8af7910df
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2008-08-28 17:38:17 +0000

    New property 'portletLocales' in xava.properties that indicates the locales to use for generating i18n files for portlets

--- trunk/OpenXava/generator/Jetspeed2FolderPG.java
+++ trunk/OpenXava/generator/Jetspeed2FolderPG.java
@@ -10,7 +10,7 @@ import org.openxava.generators.*;
 
 /**
  * Program Generator created by TL2Java
- * @version Mon May 05 12:54:38 CEST 2008
+ * @version Thu Aug 28 19:06:46 CEST 2008
  */
 public class Jetspeed2FolderPG {
     Properties properties = new Properties();
@@ -96,7 +96,7 @@ public class Jetspeed2FolderPG {
      * This array provides program generator development history
      */
     public String[][] history = {
-        { "Mon May 05 12:54:38 CEST 2008", // date this file was generated
+        { "Thu Aug 28 19:06:46 CEST 2008", // date this file was generated
              "../OpenXava/generator/jetspeed2folder.xml", // input file
              "../OpenXava/generator/Jetspeed2FolderPG.java" }, // output file
         {"Mon Apr 09 16:45:30 EDT 2001", "TL2Java.xml", "TL2Java.java", }, 
--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.93 2008-08-28 15:59:23 javierpaniza Exp $
+// $Id: calendar.js,v 1.94 2008-08-28 17:37:17 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
