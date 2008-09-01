d62bf4ca176f587c75c6c5aa58c5799832db2659
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2008-09-01 14:47:43 +0000

    Fix: Modifying the container object from a callback method of a collection element fails

--- OpenXavaTest/test-src/org/openxava/test/tests/Invoice2Test.java
+++ OpenXavaTest/test-src/org/openxava/test/tests/Invoice2Test.java
@@ -13,6 +13,63 @@ public class Invoice2Test extends ModuleTestBase {
 		super(testName, "Invoice2");		
 	}
 	
+	public void testTouchContainerFromCallback() throws Exception { 
+		execute("CRUD.new");
+		setValue("number", "66");
+		setValue("vatPercentage", "16");
+		setValue("customer.number", "1");
+		assertCollectionRowCount("details", 0);
+		
+		// Creating a new detail
+		execute("Collection.new", "viewObject=xava_view_details");
+		setValue("details.quantity", "7");
+		setValue("details.unitPrice", "8");
+		assertValue("details.amount", "56.00");
+		setValue("details.product.number", "1");
+		assertValue("details.product.description", "MULTAS DE TRAFICO");
+		execute("Collection.save", "viewObject=xava_view_details");
+		assertNoErrors();
+		assertCollectionRowCount("details", 1);
+		execute("CRUD.search");
+		assertValue("amountsSum", "56.00");
+		
+		// Creating another one
+		execute("Collection.new", "viewObject=xava_view_details");
+		setValue("details.quantity", "10");
+		setValue("details.unitPrice", "10");
+		assertValue("details.amount", "100.00");
+		setValue("details.product.number", "1");
+		assertValue("details.product.description", "MULTAS DE TRAFICO");
+		execute("Collection.save", "viewObject=xava_view_details");
+		assertNoErrors();
+		assertCollectionRowCount("details", 2);
+		execute("CRUD.search");
+		assertValue("amountsSum", "156.00");
+		
+		// Modifiying
+		execute("Collection.edit", "row=1,viewObject=xava_view_details");
+		setValue("details.quantity", "20");
+		setValue("details.unitPrice", "10");
+		execute("Collection.save", "viewObject=xava_view_details");
+		assertNoErrors();
+		assertCollectionRowCount("details", 2);
+		execute("CRUD.search");
+		assertValue("amountsSum", "256.00");
+		
+		// Removing
+		execute("Collection.edit", "row=1,viewObject=xava_view_details");
+		setValue("details.quantity", "20");
+		setValue("details.unitPrice", "10");
+		execute("Collection.remove", "viewObject=xava_view_details");
+		assertNoErrors();
+		assertCollectionRowCount("details", 1);
+		execute("CRUD.search");
+		assertValue("amountsSum", "56.00");
+		
+		execute("CRUD.delete");
+		assertNoErrors();		
+	}
+	
 	public void testInjectPropertiesOfContainerInOnCreateCalculatorOfAggregate() throws Exception {
 		execute("CRUD.new");
 		setValue("number", "66");
