8af858cd1b22af9624bbbdc32515b31da028e078
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2008-11-03 15:57:13 +0000

    Fix: In IE6 and IE6 a space on top of the module is show, also this space is incremented on click actions. For 3.1beta

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.106 2008-11-03 11:23:02 javierpaniza Exp $
+// $Id: calendar.js,v 1.107 2008-11-03 15:57:05 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
