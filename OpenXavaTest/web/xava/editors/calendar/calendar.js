dccc20a225f993408cc30908891455d1c07df2b7
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2008-11-19 17:14:18 +0000

    Fix over 3.1beta3: Liferay + MS Internet Explorer does not work

--- OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.119 2008-11-19 12:54:54 javierpaniza Exp $
+// $Id: calendar.js,v 1.120 2008-11-19 17:14:10 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
