6c2496b0cb0a45789c5e63b53b312d5720fc3c91
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2008-12-04 19:38:40 +0000

    Just before 3.1rc1

--- MiEscuela/web/xava/js/openxava.js
+++ MiEscuela/web/xava/js/openxava.js
@@ -65,6 +65,7 @@ openxava.refreshPage = function(result) {
 		}						
 		openxava.formName = "xava_form";
 		openxava.form = document.getElementById(openxava.formName);
+		if (openxava.initTheme != null) openxava.initTheme();		
 		if (result.focusPropertyId != null) { 
 			document.getElementById("xava_focus_property_id").value = result.focusPropertyId;
 			openxava.setFocus();		
@@ -74,9 +75,9 @@ openxava.refreshPage = function(result) {
 	openxava.form.xava_action.value="";	
 	openxava.form.xava_action_argv.value="";
 	openxava.form.xava_changed_property.value=""; 
-	
-	document.getElementById("xava_loading").value=false;
+		
 	document.getElementById("xava_loaded_parts").value=changed;
+	document.getElementById("xava_loading").value=false;
 	document.body.style.cursor='auto'; 
 }
 
