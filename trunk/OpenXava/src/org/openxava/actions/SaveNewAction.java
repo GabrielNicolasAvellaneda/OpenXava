dd35951d1edc3c0df3a6aa63e6881c5c7fa2a762
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-01-20 18:23:24 +0000

    Fix: Creating from a reference an entity with collection fails

--- trunk/OpenXava/src/org/openxava/actions/SaveNewAction.java
+++ trunk/OpenXava/src/org/openxava/actions/SaveNewAction.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 import javax.ejb.*;
 
-
-
 import org.openxava.model.*;
 import org.openxava.validators.*;
 
@@ -14,21 +12,73 @@ import org.openxava.validators.*;
  */
 
 public class SaveNewAction extends UpdateReferenceBaseAction {
-	
-	
-	
-	public void execute() throws Exception {		
-		try {					
-			// Create
-			Map key = MapFacade.createReturningKey(getView().getModelName(), getValuesToSave());			
+
+	public void execute() throws Exception {
+		try {
+			Map key = null;
+			if (getView().isKeyEditable()) {
+				// Create
+				key = MapFacade.createReturningKey(getView().getModelName(),
+						getValuesToSave());
+			} else {
+				// Modify, usually when collection exists, because 
+				// saving an element collection saves automatically the main entity
+				key = getView().getKeyValues();
+				MapFacade.setValues(getView().getModelName(), key,
+						getValuesToSave());
+			}
 			returnsToPreviousViewUpdatingReferenceView(key);
-		}
-		catch (ValidationException ex) {			
+		} 
+		catch (ValidationException ex) {
 			addErrors(ex.getErrors());
-		}
+		} 
 		catch (DuplicateKeyException ex) {
 			addError("no_create_exists");
 		}
+
+		/* tmp original
+		try {
+			// Create
+			Map key = MapFacade.createReturningKey(getView().getModelName(),
+					getValuesToSave());
+			returnsToPreviousViewUpdatingReferenceView(key);
+		} catch (ValidationException ex) {
+			addErrors(ex.getErrors());
+		} catch (DuplicateKeyException ex) {
+			addError("no_create_exists");
+		}
+		*/
+		// tmp ini
+		/*
+		Map values = null;
+		Map key = null;
+
+		try {
+			// Create
+			if (getView().isKeyEditable()) {
+				// Create
+				if (isResetAfter()) {
+					MapFacade.create(getView().getModelName(),
+							getValuesToSave());
+				} else {
+					key = MapFacade.createReturningKey(
+							getView().getModelName(), getValuesToSave());
+				}
+			} else {
+				// Modify
+				key = getView().getKeyValues();
+				MapFacade.setValues(getView().getModelName(), key,
+						getValuesToSave());
+			}
+
+			returnsToPreviousViewUpdatingReferenceView(key);
+		} catch (ValidationException ex) {
+			addErrors(ex.getErrors());
+		} catch (DuplicateKeyException ex) {
+			addError("no_create_exists");
+		}
+		*/
+		// tmp fin
 	}
-		
+
 }
