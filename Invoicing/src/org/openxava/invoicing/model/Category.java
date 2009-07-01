68a8323fd0e876ea60fc044ac00c168ccd553bc2
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-07-01 07:57:28 +0000

    New mapped superclass Identifiable

--- Invoicing/src/org/openxava/invoicing/model/Category.java
+++ Invoicing/src/org/openxava/invoicing/model/Category.java
@@ -2,30 +2,12 @@ package org.openxava.invoicing.model;
 
 import javax.persistence.*;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.openxava.annotations.*;
-
 @Entity
-public class Category {
-	
-	@Id @GeneratedValue(generator="system-uuid") @Hidden 
-	@GenericGenerator(name="system-uuid", strategy = "uuid")
-	@Column(length=32)
-	private String oid;
+public class Category extends Identifiable {
 	
 	@Column(length=50)
 	private String description;
 	
-	// Getters and setters
-
-	public String getOid() {
-		return oid;
-	}
-
-	public void setOid(String oid) {
-		this.oid = oid;
-	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
