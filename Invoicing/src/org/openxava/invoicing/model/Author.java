6f4f411e09ef484dc9790ce2c95c0aac363da14d
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-07-03 11:37:42 +0000

    products collection added to Author

--- Invoicing/src/org/openxava/invoicing/model/Author.java
+++ Invoicing/src/org/openxava/invoicing/model/Author.java
@@ -1,5 +1,7 @@
 package org.openxava.invoicing.model;
 
+import java.util.*;
+
 import javax.persistence.*;
 import org.openxava.annotations.*;
 
@@ -8,6 +10,18 @@ public class Author extends Identifiable {
 	
 	@Column(length=50) @Required
 	private String name;
+	
+	@OneToMany(mappedBy="author") 
+	@ListProperties("number, description, price")	
+	private Collection<Product> products;
+ 
+	public Collection<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Collection<Product> products) {
+		this.products = products;
+	}
 
 	public void setName(String name) {
 		this.name = name;
