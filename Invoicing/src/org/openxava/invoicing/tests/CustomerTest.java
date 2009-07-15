d9a27012caec957278d2fb2e1c40ba8c4c9379e6
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-07-15 09:59:57 +0000

    Testing chapter (in process)

--- Invoicing/src/org/openxava/invoicing/tests/CustomerTest.java
+++ Invoicing/src/org/openxava/invoicing/tests/CustomerTest.java
@@ -13,16 +13,29 @@ public class CustomerTest extends ModuleTestBase {
 		execute("CRUD.new");                    
 		setValue("number", "77");                     
 		setValue("name", "JUNIT Customer");
+		setValue("address.street", "JUNIT Street");
+		setValue("address.zipCode", "77555");
+		setValue("address.city", "The JUNIT city");
+		setValue("address.state", "The JUNIT state");
 		execute("CRUD.save");
 		assertNoErrors();                            
 		assertValue("number", "");                   
 		assertValue("name", "");
+		assertValue("address.street", "");
+		assertValue("address.zipCode", "");
+		assertValue("address.city", "");
+		assertValue("address.state", "");		
 		 
 		// Read
 		setValue("number", "77");
 		execute("CRUD.search");
 		assertValue("number", "77");
 		assertValue("name", "JUNIT Customer");
+		assertValue("address.street", "JUNIT Street");
+		assertValue("address.zipCode", "77555");
+		assertValue("address.city", "The JUNIT city");
+		assertValue("address.state", "The JUNIT state");
+		
 		 
 		// Update
 		setValue("name", "JUNIT Customer MODIFIED");
