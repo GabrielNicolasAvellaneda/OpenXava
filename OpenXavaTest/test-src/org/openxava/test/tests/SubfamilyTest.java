d939feba5404618e8667e6287482367b4cf6cae9
anafernandez <anafernandez@576487a9-6920-41e2-8c29-37f306983587>
2009-07-17 10:01:37 +0000

    editors.xml: add 'formatter-list'. Default apply MemoFormatter at all properties with stereotype memo.

--- OpenXavaTest/test-src/org/openxava/test/tests/SubfamilyTest.java
+++ OpenXavaTest/test-src/org/openxava/test/tests/SubfamilyTest.java
@@ -1,17 +1,27 @@
 package org.openxava.test.tests;
 
-import org.openxava.tests.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openxava.tests.ModuleTestBase;
 
 /**
  * @author Javier Paniza
  */
 
 public class SubfamilyTest extends ModuleTestBase {
+	private static Log log = LogFactory.getLog(SubfamilyTest.class);
 	
 	public SubfamilyTest(String testName) {
 		super(testName, "Subfamily");		
 	}
 	
+	public void testMemoFormatter() throws Exception {
+		assertLabelInList(3, "Remarks");
+		assertValueInList(0, 3, "DESARROLLO DE SOFTWARE");
+		assertValueInList(1, 3, "GESTION INTEGRAL DE RAYOS DE LUNAS SOBRE LOS MARES...");
+		execute("List.viewDetail", "row=1");
+		assertValue("remarks", "GESTION INTEGRAL DE RAYOS DE LUNAS SOBRE LOS MARES QUE PASAN POR EL ABISMO DE MIS MISERIAS. ME ALIMENTARA Y FORTALECIERA. DISOLVIENDO EL DINERAL COMO UN VIRUS QUE SE EXTIENDE Y SE CONTAGIA.");
+	}
 	
 	public void testSaveHiddenKeyWithSections() throws Exception {
 		assertTrue("For this test is required al least 2 families", getListColumnCount() >= 2);
