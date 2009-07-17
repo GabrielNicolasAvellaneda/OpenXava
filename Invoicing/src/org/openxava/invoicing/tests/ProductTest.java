ca5a0ae1e244b4755d3911301d9e4a20822e64f8
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-07-17 18:39:21 +0000

    ProductTest finished

--- Invoicing/src/org/openxava/invoicing/tests/ProductTest.java
+++ Invoicing/src/org/openxava/invoicing/tests/ProductTest.java
@@ -1,47 +1,81 @@
 package org.openxava.invoicing.tests;
 
+import java.math.*;
+
 import org.openxava.invoicing.model.*;
 import org.openxava.tests.*;
 import static org.openxava.jpa.XPersistence.*;
 
 public class ProductTest extends ModuleTestBase {
 	
+	private Author author;
+	private Category category;
+	private Product product1;
+	private Product product2;
+	
 	public ProductTest(String testName) {
 		super(testName, "Invoicing", "Product");				
 	}
 	
-	protected void setUp() throws Exception {
-		//assertNoProductsGreatherThan900000000();		
+	protected void setUp() throws Exception {		
 		createProducts();		
 		super.setUp();
-		//filterByProductsGreatherThan900000000();
 	}
-	
-	private void assertNoProductsGreatherThan900000000() {		
 		
-	}
-
-	public void testCreateReadUpdateDelete() throws Exception {
+	protected void tearDown() throws Exception {		
+		super.tearDown();		
+		removeProducts();
 	}
 	
+	public void testRemoveFromList() throws Exception {
+		setConditionValues(new String[] { "", "JUNIT" });
+		setConditionComparators(new String[] { "=", "contains_comparator" });
+		execute("List.filter");
+		assertListRowCount(2);
+		checkRow(1);
+		execute("CRUD.deleteSelected");		
+		assertListRowCount(1);
+	}
+	
+	public void testChagePrice() throws Exception {
+		// Searching the product1
+		execute("CRUD.new");
+		setValue("number", Integer.toString(product1.getNumber()));
+		execute("CRUD.search");
+		assertValue("price", "10.00");
+		
+		// Changing the price
+		setValue("price", "12.00");
+		execute("CRUD.save");
+		assertNoErrors();
+		assertValue("price", "");
+
+		// Verifying
+		setValue("number", Integer.toString(product1.getNumber()));
+		execute("CRUD.search");
+		assertValue("price", "12.00");
+	}	
+			
 	private void createProducts() {
 		// Creating the Java object
-		Author author = new Author();
+		author = new Author();
 		author.setName("JUNIT Author");
-		Category category = new Category();
+		category = new Category();
 		category.setDescription("JUNIT Category");
 		
-		Product product1 = new Product();
+		product1 = new Product();
 		product1.setNumber(900000001);
 		product1.setDescription("JUNIT Product 1");
 		product1.setAuthor(author);
 		product1.setCategory(category);
+		product1.setPrice(new BigDecimal("10"));
 		
-		Product product2 = new Product();
+		product2 = new Product();
 		product2.setNumber(900000002);
 		product2.setDescription("JUNIT Product 2");
 		product2.setAuthor(author);
 		product2.setCategory(category);
+		product2.setPrice(new BigDecimal("20"));
 		
 		// Marking as persistent objects
 		getManager().persist(author);
@@ -53,4 +87,15 @@ public class ProductTest extends ModuleTestBase {
 		commit();
 	}
 		
+	private void removeProducts() {
+		remove(product1, product2, author, category);
+		commit();		
+	}
+	
+	private void remove(Object ... entities) {
+		for (Object entity: entities) {
+			getManager().remove(getManager().merge(entity));
+		}
+	}		
+			
 }
