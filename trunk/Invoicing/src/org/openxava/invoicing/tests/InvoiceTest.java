f196d078586d6d9b5fe96f9680bc30b16ae8b5d4
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-08-04 18:34:15 +0000

    OrderTest and InvoiceTest finished

--- trunk/Invoicing/src/org/openxava/invoicing/tests/InvoiceTest.java
+++ trunk/Invoicing/src/org/openxava/invoicing/tests/InvoiceTest.java
@@ -5,5 +5,20 @@ public class InvoiceTest extends CommercialDocumentTest {
 	public InvoiceTest(String testName) { 
 		super(testName, "Invoice"); 				
 	}
+	
+	public void testAddOrders() throws Exception {
+		assertListNotEmpty();
+		execute("Mode.detailAndFirst");
+		execute("Sections.change", "activeSection=1");
+		assertCollectionRowCount("orders", 0);
+		execute("Collection.add", 
+			"viewObject=xava_view_section1_orders");
+		execute("AddToCollection.add", "row=0");
+		assertCollectionRowCount("orders", 1);
+		checkRowCollection("orders", 0);
+		execute("Collection.removeSelected", 
+			"viewObject=xava_view_section1_orders");
+		assertCollectionRowCount("orders", 0);
+	}
 			
 }
--- trunk/Invoicing/src/org/openxava/invoicing/tests/OrderTest.java
+++ trunk/Invoicing/src/org/openxava/invoicing/tests/OrderTest.java
@@ -5,5 +5,20 @@ public class OrderTest extends CommercialDocumentTest {
 	public OrderTest(String testName) { 
 		super(testName, "Order"); 				
 	}
+	
+	public void testSetInvoice() throws Exception {
+		assertListNotEmpty();
+		execute("Mode.detailAndFirst");
+		execute("Sections.change", "activeSection=1");
+		assertValue("invoice.number", "");
+		assertValue("invoice.year", "");
+		execute("Reference.search", 
+			"keyProperty=invoice.year");
+		String year = getValueInList(0, "year");
+		String number = getValueInList(0, "number");		
+		execute("ReferenceSearch.choose", "row=0");
+		assertValue("invoice.year", year);
+		assertValue("invoice.number", number);		
+	}
 			
 }
