0848fd81c254635cc31bfd7d8a2bba95faa36d99
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-08-19 08:37:11 +0000

    When the user reset the filters in list, the comparators (the combos) are reset too

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.159 2009-08-10 10:52:24 javierpaniza Exp $
+// $Id: calendar.js,v 1.160 2009-08-19 08:36:03 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
