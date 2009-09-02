adf1032a1e9e9357322b75128555e9afa2a28f8f
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-09-02 18:29:52 +0000

    ISBN validator advanced

--- trunk/Invoicing/src/org/openxava/invoicing/validators/ISBNValidator.java
+++ trunk/Invoicing/src/org/openxava/invoicing/validators/ISBNValidator.java
@@ -1,6 +1,6 @@
 package org.openxava.invoicing.validators;
 
-import org.apache.commons.logging.*;
+// tmp import org.apache.commons.logging.*;
 import org.hibernate.validator.*;
 import org.openxava.invoicing.annotations.*;
 
@@ -9,7 +9,7 @@ import com.gargoylesoftware.htmlunit.html.*;
 
 public class ISBNValidator implements Validator<ISBN> {
 	
-	private static Log log = LogFactory.getLog(ISBNValidator.class);
+	// tmp private static Log log = LogFactory.getLog(ISBNValidator.class);
 	private static org.apache.commons.validator.ISBNValidator validator = new org.apache.commons.validator.ISBNValidator();
 
 	public void initialize(ISBN isbn) {
@@ -17,22 +17,25 @@ public class ISBNValidator implements Validator<ISBN> {
 
 	public boolean isValid(Object value) {
 		if (value == null) return true;
-		if (!validator.isValid(value.toString())) return false;
-		return isbnExists(value); 
+		return validator.isValid(value.toString());
+		// tmp if (!validator.isValid(value.toString())) return false;
+		// return isbnExists(value); 
 	}
 
+	
 	private boolean isbnExists(Object isbn) {
 		try {
 			WebClient client = new WebClient();
 			HtmlPage page = (HtmlPage) client.getPage(
 					"http://www.bookfinder4u.com/IsbnSearch.aspx?isbn=" +
 					isbn + "&mode=direct");
-			return page.asText().indexOf("ISBN-13: " + isbn) >= 0;
+			System.out.println("[ISBNValidator.isbnExists] " + page.asText()); // tmp
+			return page.asText().indexOf("ISBN-10: " + isbn) >= 0;
 		}
 		catch (Exception ex) {
 			log.warn("Impossible to connect to bookfinder4u to validate the ISBN. Validation fails", ex);
 			return false;
 		}
-	}
+	}	
 
 }
