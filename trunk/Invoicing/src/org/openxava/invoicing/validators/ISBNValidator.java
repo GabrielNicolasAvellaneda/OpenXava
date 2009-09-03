3b3d854c963f25756821bef5b97f0274765ca0d9
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-09-03 18:34:58 +0000

    ISBN validator finished

--- trunk/Invoicing/src/org/openxava/invoicing/validators/ISBNValidator.java
+++ trunk/Invoicing/src/org/openxava/invoicing/validators/ISBNValidator.java
@@ -1,25 +1,28 @@
 package org.openxava.invoicing.validators;
 
-// tmp import org.apache.commons.logging.*;
+import org.apache.commons.logging.*;
 import org.hibernate.validator.*;
 import org.openxava.invoicing.annotations.*;
+import org.openxava.util.*;
 
 import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
 
 public class ISBNValidator implements Validator<ISBN> {
 	
-	// tmp private static Log log = LogFactory.getLog(ISBNValidator.class);
+	private static Log log = LogFactory.getLog(ISBNValidator.class);
 	private static org.apache.commons.validator.ISBNValidator validator = new org.apache.commons.validator.ISBNValidator();
+	
+	private boolean search;
 
 	public void initialize(ISBN isbn) {
+		this.search = isbn.search();
 	}
 
 	public boolean isValid(Object value) {
-		if (value == null) return true;
-		return validator.isValid(value.toString());
-		// tmp if (!validator.isValid(value.toString())) return false;
-		// return isbnExists(value); 
+		if (Is.empty(value)) return true;
+		if (!validator.isValid(value.toString())) return false;
+		return search?isbnExists(value):true; 
 	}
 
 	
@@ -29,7 +32,6 @@ public class ISBNValidator implements Validator<ISBN> {
 			HtmlPage page = (HtmlPage) client.getPage(
 					"http://www.bookfinder4u.com/IsbnSearch.aspx?isbn=" +
 					isbn + "&mode=direct");
-			System.out.println("[ISBNValidator.isbnExists] " + page.asText()); // tmp
 			return page.asText().indexOf("ISBN-10: " + isbn) >= 0;
 		}
 		catch (Exception ex) {
