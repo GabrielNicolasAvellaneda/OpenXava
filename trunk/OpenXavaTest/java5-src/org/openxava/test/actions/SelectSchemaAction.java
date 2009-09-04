c6a3831ff309bfd9066af6a1f135ba82f69cf2f2
anafernandez <anafernandez@576487a9-6920-41e2-8c29-37f306983587>
2009-09-04 10:01:10 +0000

    Fix: ModuleManager.reset not work with descriptionsList at default schema components

--- trunk/OpenXavaTest/java5-src/org/openxava/test/actions/SelectSchemaAction.java
+++ trunk/OpenXavaTest/java5-src/org/openxava/test/actions/SelectSchemaAction.java
@@ -6,30 +6,44 @@ import java.util.Iterator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openxava.actions.IModuleContextAction;
-import org.openxava.actions.ViewBaseAction;
+import org.openxava.actions.SetDefaultSchemaAction;
 import org.openxava.controller.ModuleContext;
 import org.openxava.controller.ModuleManager;
+import org.openxava.view.View;
 
 /**
- * Create on 10/08/2009 (14:01:01)
+ * Create on 03/09/2009 (14:49:04)
  * @autor Ana Andrés
  */
-public class InitModulesAction extends ViewBaseAction implements IModuleContextAction{ 
-	private static Log log = LogFactory.getLog(InitModulesAction.class);
+public class SelectSchemaAction extends SetDefaultSchemaAction implements IModuleContextAction{
+	private static Log log = LogFactory.getLog(SelectSchemaAction.class);
 	
+	private View view;
 	private ModuleContext context;
 	
 	public void setContext(ModuleContext context) {
 		this.context = context;
 	}
-
+	
 	public void execute() throws Exception {
+		setNewDefaultSchema(getView().getValueString("schema"));
+		super.execute();
+		
+		// restart all modules less SelectSchema
 		Collection managers = (Collection) context.getAll("manager");
 		Iterator it = managers.iterator();
 		while(it.hasNext()){
 			ModuleManager manager = (ModuleManager) it.next();
-			manager.reset();
+			if (!manager.getModuleName().equalsIgnoreCase("SelectSchema")) manager.reset();
 		}
+	}
+
+	public View getView() {
+		return view;
+	}
+
+	public void setView(View view) {
+		this.view = view;
 	}
 
 }
