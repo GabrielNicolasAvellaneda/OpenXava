72f80283dab4c64ec82812f096bf7aac300167b9
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2009-11-06 12:26:42 +0000

    Chapter 10 advanced

--- Invoicing/src/org/openxava/invoicing/actions/DeleteInvoiceAction.java
+++ Invoicing/src/org/openxava/invoicing/actions/DeleteInvoiceAction.java
@@ -4,9 +4,9 @@ import org.openxava.actions.*;
 
 public class DeleteInvoiceAction extends ViewBaseAction {
 
-	public void execute() throws Exception {		
-		getView().clear();
+	public void execute() throws Exception {
 		addMessage("Don't worry! I have cleared only the screen");
+		getView().clear();		
 	}
 
 }
