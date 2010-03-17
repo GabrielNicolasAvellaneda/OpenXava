4f95cc0dae7cb146a9189689ba403b452ae3ac34
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-03-17 12:50:32 +0000

    - The JavaScript function openxava.addEditorInitFunction to add code to be executed for initialized JS of custom editors. - Any JavaScript file in xava/editors/js is loaded automatically. This folder is for JS of custom editors. - The dot key can be used as decimal separator in any locale. It is converted automatically to the local decimal separator. - Fix: Alphabetic characters can be entered in numeric fields. - Attribute id included in Html for all the built-in editors, to facilitate JavaScript manipulation. - custom-editors.js is only included if exists. Moreover the usage of this file has been deprecated.

--- AccessTracking/web/xava/editors/calendar/calendar.js
+++ AccessTracking/web/xava/editors/calendar/calendar.js
@@ -13,7 +13,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.36 2010-01-27 09:33:30 javierpaniza Exp $
+// $Id: calendar.js,v 1.37 2010-03-17 12:50:25 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 if (typeof Calendar == "undefined") { // By Javier Paniza, in order to work in a multi-portlet environment
--- AccessTracking/web/xava/style/liferay51/js/barebone_unpacked.js
+++ AccessTracking/web/xava/style/liferay51/js/barebone_unpacked.js
@@ -6,7 +6,7 @@
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * $Date: 2010-01-27 09:33:30 $
+ * $Date: 2010-03-17 12:50:32 $
  * $Rev: 5685 $
  */
 
