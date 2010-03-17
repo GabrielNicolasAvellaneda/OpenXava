868f47d4711e865bbeb8c1bc591098fd476df317
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-03-17 12:04:16 +0000

    - The JavaScript function openxava.addEditorInitFunction to add code to be executed for initialized JS of custom editors. - Any JavaScript file in xava/editors/js is loaded automatically. This folder is for JS of custom editors. - The dot key can be used as decimal separator in any locale. It is converted automatically to the local decimal separator. - Fix: Alphabetic characters can be entered in numeric fields. - Attribute id included in Html for all the built-in editors, to facilitate JavaScript manipulation. - custom-editors.js is only included if exists. Moreover the usage of this file has been deprecated.

--- OpenXavaTest/pojo-src/org/openxava/test/tests/Product4Test.java
+++ OpenXavaTest/pojo-src/org/openxava/test/tests/Product4Test.java
@@ -25,5 +25,18 @@ public class Product4Test extends ModuleTestBase {
 		setValue("unitPrice", "10");
 		assertValue("unitPriceInPesetas", "1,664");		
 	}
+	
+	public void testValidationFromPrePersist() throws Exception {
+		execute("CRUD.new");
+		setValue("number", "666");
+		setValue("description", "OPENXAVA");
+		setValue("subfamily.number", "12");
+		setValue("unitPrice", "300");
+		execute("CRUD.save");
+		assertError("You cannot sell OpenXava");		
+		setValue("description", "WEBSPHERE");
+		execute("CRUD.save");
+		assertError("666 is not a valid value for Number of Product 4: It's number of man");
+	}
 		
 }
