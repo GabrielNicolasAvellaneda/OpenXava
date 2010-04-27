7814a4bbc69464614ab54d0ea165a6e1982de571
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-04-27 09:29:44 +0000

    - More intuitive 'search' action. Now it uses a dialog to ask for the search data. - New 'refresh' action in CRUD controller. Behaves as the old 'search'. - The default action for CRUD controller now is 'save'. Before it was 'search'.

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -13,7 +13,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.197 2010-04-27 09:22:58 javierpaniza Exp $
+// $Id: calendar.js,v 1.198 2010-04-27 09:29:44 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 if (typeof Calendar == "undefined") { // By Javier Paniza, in order to work in a multi-portlet environment
--- trunk/OpenXavaTest/web/xava/style/liferay51/js/barebone_unpacked.js
+++ trunk/OpenXavaTest/web/xava/style/liferay51/js/barebone_unpacked.js
@@ -6,7 +6,7 @@
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * $Date: 2010-04-27 09:22:57 $
+ * $Date: 2010-04-27 09:29:44 $
  * $Rev: 5685 $
  */
 
