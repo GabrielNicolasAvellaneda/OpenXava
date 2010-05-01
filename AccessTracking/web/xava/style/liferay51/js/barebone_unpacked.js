611aa5681ed4635c9b5d7eb9890cb311dd6ceaf1
falcantara <falcantara@576487a9-6920-41e2-8c29-37f306983587>
2010-05-01 11:55:06 +0000

    -Diff caused by OpenXava changes (4m4 beta).

--- AccessTracking/web/xava/style/liferay51/js/barebone_unpacked.js
+++ AccessTracking/web/xava/style/liferay51/js/barebone_unpacked.js
@@ -6,7 +6,7 @@
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * $Date: 2010-03-29 09:09:41 $
+ * $Date: 2010-05-01 11:55:06 $
  * $Rev: 5685 $
  */
 
