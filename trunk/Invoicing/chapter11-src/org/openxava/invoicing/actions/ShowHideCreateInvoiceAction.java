3ce4934d9dc216a9f55f8ffd6e4bc2c3faab65fa
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-05-03 16:31:27 +0000

    Chapter 11 finished

---
+++ trunk/Invoicing/chapter11-src/org/openxava/invoicing/actions/ShowHideCreateInvoiceAction.java
@@ -0,0 +1,37 @@
+package org.openxava.invoicing.actions;
+
+import org.openxava.actions.*;
+
+public class ShowHideCreateInvoiceAction 
+	extends OnChangePropertyBaseAction 
+	implements IShowActionAction, 
+		IHideActionAction {
+	
+	private boolean show;
+
+	public void execute() throws Exception {
+		show =  isOrderCreated() && isDelivered() && !hasInvoice();		
+	}
+
+	private boolean isOrderCreated() {
+		return getView().getValue("oid") != null;
+	}
+	
+	private boolean isDelivered() {
+		Boolean delivered = (Boolean) getView().getValue("delivered");
+		return delivered == null?false:delivered;
+	}
+	
+	private boolean hasInvoice() {
+		return getView().getValue("invoice.oid") != null;
+	}
+
+	public String getActionToShow() {		
+		return show?"Order.createInvoice":"";
+	}
+
+	public String getActionToHide() {
+		return !show?"Order.createInvoice":"";
+	}
+	
+}
