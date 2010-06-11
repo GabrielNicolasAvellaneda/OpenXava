8821817a09b5b0ef2d2867bd22633620e130f851
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-06-11 15:44:37 +0000

    Chapter 12 finished

---
+++ Invoicing/chapter12-src/org/openxava/invoicing/model/Detail.java
@@ -0,0 +1,91 @@
+package org.openxava.invoicing.model;
+
+import java.math.*;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+import org.openxava.invoicing.calculators.*;
+
+@Entity
+@View(members="product; quantity, pricePerUnit, amount")  
+public class Detail extends Identifiable {
+	
+	@ManyToOne // Lazy fetching fails on removing a detail from parent
+	private CommercialDocument parent;
+		
+	private int quantity;
+	
+	@ManyToOne(fetch=FetchType.LAZY, optional=true)
+	@ReferenceView("Simple")
+	@NoFrame
+	private Product product;
+	
+	@DefaultValueCalculator(value=PricePerUnitCalculator.class,
+		properties=@PropertyValue(
+				name="productNumber", 
+				from="product.number")
+	)
+	@Stereotype("MONEY")
+	private BigDecimal pricePerUnit;	
+
+	@Stereotype("MONEY")  
+	@Depends("pricePerUnit, quantity") 
+	public BigDecimal getAmount() {
+		return new BigDecimal(quantity).multiply(getPricePerUnit());
+	}
+	
+	@PrePersist  
+	private void onPersist() {
+		getParent().getDetails().add(this);
+		getParent().recalculateAmount();
+	}
+	
+	@PreUpdate
+	private void onUpdate() {
+		getParent().recalculateAmount();
+	}	
+	
+	
+	@PreRemove
+	private void onRemove() {
+		if (getParent().isRemoving()) return; 		
+		getParent().getDetails().remove(this);
+		getParent().recalculateAmount();
+	}
+	
+	// Getters and setters
+	
+	public CommercialDocument getParent() {
+		return parent;
+	}
+
+	public void setParent(CommercialDocument parent) {
+		this.parent = parent;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	public BigDecimal getPricePerUnit() {
+		return pricePerUnit==null?BigDecimal.ZERO:pricePerUnit; 
+	}
+
+	public void setPricePerUnit(BigDecimal pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+	
+}
---
+++ Invoicing/chapter12-src/org/openxava/invoicing/model/Invoice.java
@@ -0,0 +1,62 @@
+package org.openxava.invoicing.model;
+
+import java.util.*;
+
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import org.openxava.validators.*;
+
+@Entity 
+@Views({
+	@View( extendsView="super.DEFAULT",
+		members="orders { orders } "
+	),	
+	@View( name="NoCustomerNoOrders", members=			
+		"year, number, date;" +
+		"details;" +
+		"remarks" 
+	)
+	
+})
+@Tabs({
+	@Tab(
+		baseCondition = "deleted = false", 
+		properties="year, number, date, customer.number, customer.name," +
+			"vatPercentage, estimatedProfit, baseAmount, " +
+			"vat, totalAmount, amount, remarks"),
+	@Tab(name="Deleted", baseCondition = "deleted = true")
+})
+public class Invoice extends CommercialDocument {
+	
+	@OneToMany(mappedBy="invoice")
+	@CollectionView("NoCustomerNoInvoice")
+	@NewAction("Invoice.addOrders")
+	private Collection<Order> orders;
+	
+	public Collection<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Collection<Order> orders) {
+		this.orders = orders;
+	}
+
+	public static Invoice createFromOrders(Collection<Order> orders) throws ValidationException { 
+		Invoice invoice = null;
+		for (Order order: orders) {
+			if (invoice == null) {
+				order.createInvoice();
+				invoice = order.getInvoice();
+			}
+			else {
+				order.setInvoice(invoice);
+				order.copyDetailsToInvoice(invoice);
+			}
+		}
+		if (invoice == null) {
+			throw new ValidationException("impossible_create_invoice_orders_not_specified");
+		}
+		return invoice;
+	}
+	
+}
