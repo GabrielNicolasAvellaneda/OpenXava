41a2cab55f1cf411db7333775a0407808c990057
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-07-14 11:55:49 +0000

    Declared explicitly HibernatePersistence as provider for JPA

--- MySchool/web/xava/style/liferay51/js/barebone_unpacked.js
+++ MySchool/web/xava/style/liferay51/js/barebone_unpacked.js
@@ -6,7 +6,7 @@
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * $Date: 2010-05-25 12:19:58 $
+ * $Date: 2010-07-14 11:55:37 $
  * $Rev: 5685 $
  */
 
