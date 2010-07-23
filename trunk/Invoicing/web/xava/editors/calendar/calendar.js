44198bcf1c59103cc727f863cfda3add8074aca6
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-07-23 16:03:32 +0000

    Chapter 13 advanced

--- trunk/Invoicing/web/xava/editors/calendar/calendar.js
+++ trunk/Invoicing/web/xava/editors/calendar/calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.6 2010-07-16 16:13:13 javierpaniza Exp $
+// $Id: calendar.js,v 1.7 2010-07-23 16:03:32 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
