6fadd29ed51615b257b7252a461d364cb4e64b44
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-07-26 12:40:16 +0000

    Tested with liferay

--- OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -13,7 +13,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.220 2010-07-26 12:14:20 javierpaniza Exp $
+// $Id: calendar.js,v 1.221 2010-07-26 12:40:16 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 if (typeof Calendar == "undefined") { // By Javier Paniza, in order to work in a multi-portlet environment
