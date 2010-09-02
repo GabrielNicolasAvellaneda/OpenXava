6d2bd8d194ac35a36c79929bcb0e694d0c12d275
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-09-02 10:03:24 +0000

    Chapter 13 finished

---
+++ trunk/Invoicing/chapter13-src/org/openxava/invoicing/tests/CategoryTest.java
@@ -0,0 +1,18 @@
+package org.openxava.invoicing.tests;
+
+import org.openxava.tests.*;
+
+public class CategoryTest extends ModuleTestBase {
+	
+	public CategoryTest(String testName) {
+		super(testName, "Invoicing", "Category");				
+	}
+		
+	public void testCategoriesInList() throws Exception {
+		assertValueInList(0, 0, "MUSIC");
+		assertValueInList(1, 0, "BOOKS");
+		assertValueInList(2, 0, "SOFTWARE");
+	}
+		
+}
+
