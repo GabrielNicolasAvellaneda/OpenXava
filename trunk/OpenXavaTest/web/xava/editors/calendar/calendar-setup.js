3d71a8e105a4893b192f2dfaa2c38750def3d04f
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2010-09-17 09:26:49 +0000

    - New editor DocumentLibrary (and stereotype DOCUMENT_LIBRARY) to have a group of documents stored in Liferay Document Library. - Fix: NullPointerException by EditorTag hides the real source of error in some cases.

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
@@ -19,7 +19,7 @@
  * than modifying calendar.js itself).
  */
 
-// $Id: calendar-setup.js,v 1.228 2010-09-15 09:28:08 javierpaniza Exp $
+// $Id: calendar-setup.js,v 1.229 2010-09-17 09:25:44 javierpaniza Exp $
 
 /**
  *  This function "patches" an input field (or other element) to use a calendar
