6fcd705315a59e76bf16305c3d49ccd573aa7a8e
falcantara <falcantara@576487a9-6920-41e2-8c29-37f306983587>
2010-12-30 04:02:41 +0000

    Tests for @PreCreate, @PostCreate and @PreDelete

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -13,7 +13,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.250 2010-12-23 11:23:41 javierpaniza Exp $
+// $Id: calendar.js,v 1.251 2010-12-30 04:02:41 falcantara Exp $
 
 /** The Calendar object constructor. */
 if (typeof Calendar == "undefined") { // By Javier Paniza, in order to work in a multi-portlet environment
--- trunk/OpenXavaTest/web/xava/style/liferay51/js/barebone_unpacked.js
+++ trunk/OpenXavaTest/web/xava/style/liferay51/js/barebone_unpacked.js
@@ -6,7 +6,7 @@
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * $Date: 2010-12-23 11:23:41 $
+ * $Date: 2010-12-30 04:02:41 $
  * $Rev: 5685 $
  */
 
