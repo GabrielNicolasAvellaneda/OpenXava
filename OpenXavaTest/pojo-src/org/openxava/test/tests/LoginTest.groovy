a9c6c59cbff2f512871479e01f25aa3bfa0bd190
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2011-01-14 10:37:53 +0000

    - New stereotype PASSWORD/CONTRASENA. - When the model is transient the default controller-mode for the module is DetailOnly.

---
+++ OpenXavaTest/pojo-src/org/openxava/test/tests/LoginTest.groovy
@@ -0,0 +1,32 @@
+package org.openxava.test.tests;
+
+import org.openxava.tests.*;
+
+/**
+ * @author Javier Paniza
+ */
+
+class LoginTest extends ModuleTestBase {
+	
+	LoginTest(String testName) {
+		super(testName, "Login")		
+	}
+	
+	void testPasswordStereotype_defaultModuleForTransientClassIsDetailOnly() {
+		assertNoAction "Mode.list"
+		assertNoAction "Mode.detailAndFirst"
+		assertNoAction "Mode.split"
+	
+		setValue "user", "JAVI"
+		setValue "password", "x942JlmkK"
+		execute "Login.login"
+		assertErrorsCount 1
+		
+		setValue "user", "JAVI"
+		setValue "password", "x8Hjk37mm"
+		execute "Login.login"
+		assertNoErrors()
+		assertMessage "OK"
+	}
+	
+}
