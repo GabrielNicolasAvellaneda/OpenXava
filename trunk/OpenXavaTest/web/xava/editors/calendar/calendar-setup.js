6e377c3908c95b8eb37924801e1cafe60bd6ae06
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2011-02-11 10:56:04 +0000

    - The user can add and remove summation for any numeric persistent property in list and collections. - Standard PDF reports show column summations. - Hibernate upgraded to 3.6.1. - Removed the unused library: jboss-archive-browsing.jar.

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
@@ -19,7 +19,7 @@
  * than modifying calendar.js itself).
  */
 
-// $Id: calendar-setup.js,v 1.258 2011-02-11 10:20:20 javierpaniza Exp $
+// $Id: calendar-setup.js,v 1.259 2011-02-11 10:56:04 javierpaniza Exp $
 
 /**
  *  This function "patches" an input field (or other element) to use a calendar
