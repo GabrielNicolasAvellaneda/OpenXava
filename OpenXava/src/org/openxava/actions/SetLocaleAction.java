48a186353cd902ba9ccad7bfaa965dc7ebfcfd24
falcantara <falcantara@576487a9-6920-41e2-8c29-37f306983587>
2011-03-02 19:29:41 +0000

    -Language and Country implemented in UrlParameters.

--- OpenXava/src/org/openxava/actions/SetLocaleAction.java
+++ OpenXava/src/org/openxava/actions/SetLocaleAction.java
@@ -1,19 +1,28 @@
 package org.openxava.actions;
 
-import org.openxava.actions.BaseAction;
+import java.util.Locale;
+
+import org.openxava.util.Is;
 import org.openxava.util.Locales;
 
 /**
- * 
+ * Changes locale based on url parameter.
+ * 2011/mar/02 accepts language_COUNTRY for proper locale creation.
  * @author Federico Alcantara 
  */
 
 public class SetLocaleAction extends BaseAction {
 	public void execute() throws Exception {
-		String locale = getRequest().getParameter("locale");
-		if (locale != null && !locale.equals("")) {
+		String language = getRequest().getParameter("locale");
+		String country = null;
+		if (!Is.emptyString(language)) {
+			if (language.contains("_")) {
+				country = language.split("_")[1].trim();
+				language = language.split("_")[0].trim();
+			}
+			Locale locale = Is.emptyString(country) ? new Locale(language) : new Locale(language, country);
 			getRequest().getSession().setAttribute("xava.portal.locale",
-					new java.util.Locale(locale));
+					locale);
 			Locales.setCurrent(getRequest());
 		}
 	}
