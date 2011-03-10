c583e68e5ecf0ca21bb3541ae670351715b38263
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2011-03-10 16:46:45 +0000

    - Photos are shown in list and collections. - Fix: Shorting columns if the value contains '-' produces two lines in calculated collections. - New ManyToMany.edit action for updating elements from a many to many collection.

--- OpenXavaTest/pojo-src/org/openxava/test/model/Community.java
+++ OpenXavaTest/pojo-src/org/openxava/test/model/Community.java
@@ -12,13 +12,13 @@ import org.openxava.annotations.*;
  */
 
 @Entity
-@View(name="EditableMembers") // tmp
+@View(name="EditableMembers") 
 public class Community extends Nameable {
 	
 	@ManyToMany
 	@ListAction("ManyToMany.new")
-	@EditAction(forViews="EditableMembers", value="ManyToMany.edit") // tmp
-	@OrderBy("id asc") // tmp
+	@EditAction(forViews="EditableMembers", value="ManyToMany.edit") 
+	@OrderBy("id asc") 
 	private Collection<Human> members;
 
 	public void setMembers(Collection<Human> members) {
