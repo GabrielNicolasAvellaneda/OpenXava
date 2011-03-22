b86df78d20dc04edc04b6e632b909097b03ad058
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2011-03-22 10:55:07 +0000

    Fix: Removing a collection element from detail dialog does not ask for confirmation

--- trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
+++ trunk/OpenXavaTest/web/xava/editors/calendar/calendar.js
@@ -13,7 +13,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.268 2011-03-17 12:36:06 javierpaniza Exp $
+// $Id: calendar.js,v 1.269 2011-03-22 10:55:01 javierpaniza Exp $
 
 /** The Calendar object constructor. */
 if (typeof Calendar == "undefined") { // By Javier Paniza, in order to work in a multi-portlet environment
