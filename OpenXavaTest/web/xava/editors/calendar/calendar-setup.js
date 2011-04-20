2a1fd3569360c6e49afa9ef39e27294766aa82e6
falcantara <falcantara@576487a9-6920-41e2-8c29-37f306983587>
2011-04-20 15:01:31 +0000

    JasperReports upgraded to version 4.0.2.

--- OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
+++ OpenXavaTest/web/xava/editors/calendar/calendar-setup.js
@@ -19,7 +19,7 @@
  * than modifying calendar.js itself).
  */
 
-// $Id: calendar-setup.js,v 1.273 2011-04-12 08:30:30 javierpaniza Exp $
+// $Id: calendar-setup.js,v 1.274 2011-04-20 15:01:31 falcantara Exp $
 
 /**
  *  This function "patches" an input field (or other element) to use a calendar
--- OpenXavaTest/web/xava/style/liferay51/js/barebone_unpacked.js
+++ OpenXavaTest/web/xava/style/liferay51/js/barebone_unpacked.js
@@ -6,7 +6,7 @@
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * $Date: 2011-04-12 08:30:30 $
+ * $Date: 2011-04-20 15:01:31 $
  * $Rev: 5685 $
  */
 
