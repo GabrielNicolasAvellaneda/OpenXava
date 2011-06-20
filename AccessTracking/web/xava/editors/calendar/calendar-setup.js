d8ffa1d5bd3649be3b42212cf0a2524a480a5576
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2011-06-20 09:31:13 +0000

    Just before v4.2

--- AccessTracking/web/xava/editors/calendar/calendar-setup.js
+++ AccessTracking/web/xava/editors/calendar/calendar-setup.js
@@ -19,7 +19,7 @@
  * than modifying calendar.js itself).
  */
 
-// $Id: calendar-setup.js,v 1.48 2011-04-12 08:31:39 javierpaniza Exp $
+// $Id: calendar-setup.js,v 1.49 2011-06-20 09:31:13 javierpaniza Exp $
 
 /**
  *  This function "patches" an input field (or other element) to use a calendar
