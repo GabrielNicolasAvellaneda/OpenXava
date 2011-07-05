05d6624115ce86b21718c0725061cde0b6a9d821
anafernandez <anafernandez@576487a9-6920-41e2-8c29-37f306983587>
2011-07-05 06:22:12 +0000

    Fix: Loss of the selected rows when enter on the list to choose a reference.

--- OpenXavaTest/src/org/openxava/test/tests/DriverTest.java
+++ OpenXavaTest/src/org/openxava/test/tests/DriverTest.java
@@ -23,5 +23,26 @@ public class DriverTest extends ModuleTestBase {
 		assertValueInList(0, 0, "C");
 		assertValueInList(1, 0, "C");
 	}
+	
+	public void testNavigationBySelected() throws Exception {
+		changeModule("DriverWithTabName");
+		assertLabelInList(0, "Name");
+		assertValueInList(0, 0, "ALONSO");
+		assertValueInList(2, 0, "LORENZO");
+		checkRow(0);
+		checkRow(2);
+		
+		execute("List.viewDetail", "row=0");
+		assertValue("name", "ALONSO");
+		execute("Navigation.next");
+		assertValue("name", "LORENZO");
+		execute("Navigation.previous");
+		assertValue("name", "ALONSO");
+		// it fails when you go inside the list to choose reference
+		execute("Reference.search", "keyProperty=drivingLicence.level");
+		execute("ReferenceSearch.cancel");
+		execute("Navigation.next");
+		assertValue("name", "LORENZO");
+	}
 			
 }
