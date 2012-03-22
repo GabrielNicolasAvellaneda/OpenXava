ce0085bf1434cf7a16e51744cff62bf5eb44fe62
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2012-03-22 11:29:48 +0000

    Just before v4.4

---
+++ OpenXavaTest/pojo-src/org/openxava/test/model/City.java
@@ -0,0 +1,48 @@
+package org.openxava.test.model;
+
+import javax.persistence.*;
+import org.openxava.annotations.*;
+
+/**
+ * Create on 16/01/2012 (09:54:11)
+ * @author Ana Andres
+ */
+@IdClass(CityKey.class)
+@Entity
+public class City {
+	
+	@Id 
+	@ManyToOne(fetch=FetchType.LAZY) 
+	@DescriptionsList
+	@JoinColumn(name="STATE", referencedColumnName="ID")
+	private State state;
+	
+	@Id
+	private int code;
+	
+	private String name;
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
