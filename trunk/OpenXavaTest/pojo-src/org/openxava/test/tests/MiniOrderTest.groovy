f85f6868950080273ae7713af1cffe414f342ee3
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2012-07-04 07:43:31 +0000

    Revised version of ABL (with no log4j dependency)

--- trunk/OpenXavaTest/pojo-src/org/openxava/test/tests/MiniOrderTest.groovy
+++ trunk/OpenXavaTest/pojo-src/org/openxava/test/tests/MiniOrderTest.groovy
@@ -15,9 +15,16 @@ class MiniOrderTest extends ModuleTestBase {
 		setValue "productPrice", "2"
 		setValue "qtyOrdered", "3"
 		execute "CRUD.save"
+		assertNoErrors()
+		assertValue "number", ""
 		setValue "number", "66"
 		execute "CRUD.refresh"
 		assertValue "amount", "6"
+		
+		// Delete
+		execute "CRUD.delete"
+		assertNoErrors()
+		assertMessage "Mini order deleted successfully"
 	}
 				
 }
