8f99d514f78b376dffa3656e39b25d7c969cdd8b
falcantara <falcantara@576487a9-6920-41e2-8c29-37f306983587>
2012-08-24 20:42:44 +0000

    -Bug 3546356 corrected. Failed to parse entities annotated in the properties (property based annotation).

--- OpenXava/src/org/openxava/annotations/Tree.java
+++ OpenXava/src/org/openxava/annotations/Tree.java
@@ -19,8 +19,25 @@ import java.lang.annotation.Target;
  * &nbsp;@ListProperties("description")
  * &nbsp;@OrderBy("path, treeOrder")
  * &nbsp;private Collection<TreeItem> treeItems;
- * </pre>	
- *  
+ * </pre>
+ * <br/>
+ * The full syntax for @Tree is @Tree(forViews="", notForViews="", pathProperty="path", idProperties="", idSeparator=",", initialExpandedState=true, orderIncrement=2, pathSeparator="/").
+ * <ol>
+ * <li><b>forViews</b>. Indicates in which views the tree behavior is going to be applied.</li>
+ * <li><b>notForViews</b>. Views which are excluded from the tree renderization.</li>
+ * <li><b>pathProperty</b>. Indicates the property to be used as the path, by default points to "path". 
+ * You can customize this property if the entity path reference is persisted in a property with another name.</li>
+ * <li><b>idProperties</b>. The tree implementation needs a unique identification for each of the elements displayed. 
+ * By default the tree implementation use the properties annotated with @Id, 
+ * but you can define which properties the tree must use as the unique identifier. 
+ * The defined properties are comma separated.</li>
+ * <li><b>initialExpandedState</b>. If true the tree is rendered with all nodes expanded.</li>
+ * <li><b>orderIncrement</b>. If you define a orderBy and the orderBy is an integer type, 
+ * the Tree implementation uses this field to allow reordering of the tree elements, 
+ * by default the increment value is 2 and is the minimum allowed. 
+ * This value allow easy reordering of elements.</li>
+ * <li><b>pathSeparator</b>. If you use a separator for your path different than the default "/". 
+ * Then you can set this property to the value that you are using.</li>
  * @author Federico Alcantara
  *
  */
@@ -82,7 +99,7 @@ public @interface Tree {
 
 	/**
 	 * Optional.
-	 * Defines the increment used for the keys when orderProperty is used
+	 * Defines the increment used for the keys when orderBy is used
 	 * The default value is 2. The minimum is 2.
 	 * @return order increment.
 	 */
