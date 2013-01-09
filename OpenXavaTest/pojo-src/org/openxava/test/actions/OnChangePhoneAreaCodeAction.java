e1e9687cccb4e80b324e250cf35b10146df3d253
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2013-01-09 09:47:22 +0000

    Just before v4.6.1

--- OpenXavaTest/pojo-src/org/openxava/test/actions/OnChangePhoneAreaCodeAction.java
+++ OpenXavaTest/pojo-src/org/openxava/test/actions/OnChangePhoneAreaCodeAction.java
@@ -9,7 +9,6 @@ import org.openxava.actions.*;
 public class OnChangePhoneAreaCodeAction extends OnChangePropertyBaseAction {
 
 	public void execute() throws Exception {
-		System.out.println("[OnChangePhoneAreaCodeAction.execute] ModelName=" + getView().getModelName()); // tmp
 		if (new Integer(34).equals(getNewValue())) {
 			getView().setHidden("phoneExtension", true);
 			
