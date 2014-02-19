e1d2a59debb386c26116f953fe0469277de7408c
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2014-02-19 10:01:30 +0000

    - Upgrade to Servlet 2.5 and JSP 2.1. - Dropped Tomcat 5.5 support. - ejb.jar and jpa.jar added to WEB-INF/lib to facilitate deploying in Tomcat.

--- OpenXavaTest/pojo-src/org/openxava/test/model/ConversionFactor.java
+++ OpenXavaTest/pojo-src/org/openxava/test/model/ConversionFactor.java
@@ -5,8 +5,6 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.validator.Digits;
