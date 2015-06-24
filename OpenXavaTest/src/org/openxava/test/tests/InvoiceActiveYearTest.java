7ee0b08d3dd11eec0e37e98716b95fe974e808a4
javierpaniza <javierpaniza@576487a9-6920-41e2-8c29-37f306983587>
2015-06-24 09:09:18 +0000

    Fix: @ReadOnly does not work for views with inheritance inside groups or sections

--- OpenXavaTest/src/org/openxava/test/tests/InvoiceActiveYearTest.java
+++ OpenXavaTest/src/org/openxava/test/tests/InvoiceActiveYearTest.java
@@ -33,7 +33,7 @@ public class InvoiceActiveYearTest extends ModuleTestBase {
 	
 	public void testInjectInOnChangeAction() throws Exception {
 		execute("CRUD.new");
-		setValue("year", "2012");
+		setValue("year", "2012"); 
 		assertError("Active year is 2004, so 2012 is not allowed");
 	}
 	
